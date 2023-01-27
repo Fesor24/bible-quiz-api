029899f8e2d4e61b1754375b37e4b7c25da6476a
Wale <onafesowale@gmail.com>
2023-01-26 22:07:56 -0800

    ApplicationDbContext

--- BibleQuiz.Core/Data/ApplicationDbContext.cs
+++ BibleQuiz.Core/Data/ApplicationDbContext.cs
@@ -1,6 +1,21 @@
-﻿namespace BibleQuiz.Core
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace BibleQuiz.Core
 {
-    public class ApplicationDbContext
+    /// <summary>
+    /// Db context class
+    /// </summary>
+    public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
