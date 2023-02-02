3a723e91573b71e8f7e8819f055f0e20bc2d904a
Wale <onafesowale@gmail.com>
2023-02-02 00:34:15 -0800

    Thousand quiz questions data model

---
+++ BibleQuiz.Core/DataModels/ThousandQuizQuestionsDataModel.cs
@@ -0,0 +1,20 @@
+﻿namespace BibleQuiz.Core
+{
+    public class ThousandQuizQuestionsDataModel
+    {
+        /// <summary>
+        /// The identifier for this question
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The question
+        /// </summary>
+        public string Question { get; set; }
+
+        /// <summary>
+        /// The answer to this questions
+        /// </summary>
+        public string Answer { get; set; }
+    }
+}
