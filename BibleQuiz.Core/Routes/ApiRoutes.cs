2655587329cccbb589f78d8417c6336ac32df39c
Wale <onafesowale@gmail.com>
2023-02-02 00:35:06 -0800

    ApiRoutes

---
+++ BibleQuiz.Core/Routes/ApiRoutes.cs
@@ -0,0 +1,10 @@
+﻿namespace BibleQuiz.Core
+{
+    public static class ApiRoutes
+    {
+        /// <summary>
+        /// Endpoint to fetch questions from thousand questions by id
+        /// </summary>
+        public const string FetchThousandQuestionById = "api/thousand-question/{id}";
+    }
+}
