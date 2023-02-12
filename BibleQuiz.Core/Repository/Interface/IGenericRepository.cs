b3c0af40c57bd7c808fbf158a946e03ef80ca661
Wale <onafesowale@gmail.com>
2023-02-12 09:57:52 -0800

    IGeneric repository interface

---
+++ BibleQuiz.Core/Repository/Interface/IGenericRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibleQuiz.Core
+{
+    /// <summary>
+    /// Respository interface
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IGenericRepository<T> where T : class
+    {
+        /// <summary>
+        /// Get question by the id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<T> GetQuestionByIdAsync(int id);
+
+        /// <summary>
+        /// Get all questions
+        /// </summary>
+        /// <returns></returns>
+        Task<IReadOnlyList<T>> GetAllQuestionsAsync();
+    }
+}
