17266ca072db87c73e8719015e45da6f28495cc5
Wale <onafesowale@gmail.com>
2023-02-12 09:59:06 -0800

    Global error handling

---
+++ BibleQuiz.API/Exception/ExceptionMiddleware.cs
@@ -0,0 +1,66 @@
+﻿using System.Net;
+using System.Text.Json;
+using BibleQuiz.Core;
+
+namespace BibleQuiz.API
+{
+    /// <summary>
+    /// Exception middleware-- global error handling
+    /// </summary>
+    public class ExceptionMiddleware
+    {
+        /// <summary>
+        /// DI instance of RequestDelegate
+        /// </summary>
+        private readonly RequestDelegate next;
+
+        /// <summary>
+        /// DI instance of ILogger
+        /// </summary>
+        private readonly ILogger<ExceptionMiddleware> logger;
+
+        /// <summary>
+        /// DI instance of IHostEnvironment
+        /// </summary>
+        private readonly IHostEnvironment env;
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            this.next = next;
+            this.logger = logger;
+            this.env = env;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                // If no error encountered, pass request to next middleware
+                await next(context);
+            }
+            catch(Exception ex)
+            {
+                // Log the error to console depending on env
+                logger.LogError(ex, ex.Message);
+
+                // Set status code to 500
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                // Set content type to json
+                context.Response.ContentType = "application/json";
+
+                // Get the error details
+                var errorDetails = env.IsDevelopment() ? new ApiResponse { ErrorMessage = $"Message: {ex.Message}.{Environment.NewLine} StackTrace: {ex.StackTrace}"} : 
+                    new ApiResponse { ErrorMessage = ex.Message };
+
+                // Set json options
+                var jsonOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+                // Serialize the error response
+                var response = JsonSerializer.Serialize(errorDetails, jsonOptions);
+
+                // Pass response to response body
+                await context.Response.WriteAsync(response);
+            }
+        }
+    }
+}
