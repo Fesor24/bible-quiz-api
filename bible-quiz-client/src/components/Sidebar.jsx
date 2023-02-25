2ae1a16821e5f0f34d9ca17d5477d9b3ca3fc281
Wale <onafesowale@gmail.com>
2023-02-25 10:30:20 -0800

    Sidebar component

---
+++ bible-quiz-client/src/components/Sidebar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import style from "../styles/Sidebar.module.css";
+import Circle from "../components/Circle";
+
+function Sidebar({ correct, wrong, remaining, total }) {
+  return (
+    <div className={style.container}>
+      <div className={style.sidebar}>
+        <div className={style.content}>
+          <p>Questions answered correctly</p>
+          <Circle circleText={correct} />
+        </div>
+
+        <div className={style.content}>
+          <p>Questions answered wrongly</p>
+          <Circle circleText={wrong} />
+        </div>
+
+        <div className={style.content}>
+          <p>Questions remaining</p>
+          <Circle circleText={remaining} />
+        </div>
+
+        <div className={style.content}>
+          <p>Total questions</p>
+          <Circle circleText={total} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
