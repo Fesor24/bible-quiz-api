f5df5d6a1b07d1447ce1d6f4ca65c4ff3f8235ad
Wale <onafesowale@gmail.com>
2023-02-27 11:06:30 -0800

    Added styling for hidden buttons and props

--- bible-quiz-client/src/components/Button.jsx
+++ bible-quiz-client/src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import style from "../styles/Button.module.css";
 
-function Button({ name, click, children, disabled }) {
+function Button({ name, click, children, disabled, color, backgroundColor }) {
   return (
-    <button class={style.btn} disabled ={disabled} onClick={click}>
+    <button class={style.btn} disabled ={disabled} onClick={click} style= {{color: color, backgroundColor:backgroundColor}}>
       {name} {children}
     </button>
   );
