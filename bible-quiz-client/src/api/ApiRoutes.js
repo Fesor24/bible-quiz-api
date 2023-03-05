971accc917b2df50f13c73d543def46ba60a35bb
Wale <onafesowale@gmail.com>
2023-03-04 21:56:51 -0800

    Added routes to api routes

--- bible-quiz-client/src/api/ApiRoutes.js
+++ bible-quiz-client/src/api/ApiRoutes.js
@@ -2,4 +2,8 @@ export default class ApiRoutes {
   static BASE_URL = "https://localhost:7230/";
 
   static FetchThousandQuestions = "api/thousand-questions/fetch";
+
+  static AddRevisionQuestion = "api/revision-question/add";
+
+  static FetchRevisionQuestions = "api/revision-questions/fetch";
 }
