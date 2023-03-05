ef0f62a860394558a7b915f039a642796ecb14fd
Wale <onafesowale@gmail.com>
2023-03-04 22:00:44 -0800

    Revision question api model

---
+++ BibleQuiz.Core/ApiModels/RevisionQuestionApiModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BibleQuiz.Core
+{
+	public class RevisionQuestionApiModel
+	{
+		/// <summary>
+		/// The identifier for this question
+		/// </summary>
+		[Required]
+		public int Id { get; set; }
+
+		/// <summary>
+		/// The question
+		/// </summary>
+		[Required]
+		public string Question { get; set; }
+
+		/// <summary>
+		/// The answer to this questions
+		/// </summary>
+		[Required]
+		public string Answer { get; set; }
+	}
+}
