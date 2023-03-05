0472fcbd0eeedb97439e4edcaec57b6f8c70827e
Wale <onafesowale@gmail.com>
2023-03-04 22:02:21 -0800

    Added rutes

--- BibleQuiz.Core/Routes/ApiRoutes.cs
+++ BibleQuiz.Core/Routes/ApiRoutes.cs
@@ -11,5 +11,15 @@
         /// Endpoint to fetch all questions from ThousandQuestions
         /// </summary>
         public const string FetchThousandQuestions = "api/thousand-questions/fetch";
+
+        /// <summary>
+        /// Endpoint to add a question to the revision table
+        /// </summary>
+        public const string AddRevisionQuestion = "api/revision-question/add";
+
+        /// <summary>
+        /// Endpoint to fetch all revision questions
+        /// </summary>
+        public const string FetchRevisionQuestions = "api/revision-questions/fetch";
     }
 }
