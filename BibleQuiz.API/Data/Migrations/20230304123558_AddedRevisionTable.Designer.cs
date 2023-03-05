5f654e93d34e66eda9557b29e4d142e1906ce709
Wale <onafesowale@gmail.com>
2023-03-04 21:59:32 -0800

    Migration files

--- BibleQuiz.API/Data/Migrations/20230304123558_AddedRevisionTable.Designer.cs
+++ BibleQuiz.API/Data/Migrations/20230304123558_AddedRevisionTable.Designer.cs
@@ -4,6 +4,7 @@ using BibleQuiz.Core;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BibleQuiz.API.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230304123558_AddedRevisionTable")]
+    partial class AddedRevisionTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
