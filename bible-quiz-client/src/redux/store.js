a78365e836af77dd78160059c53771b873524628
Wale <onafesowale@gmail.com>
2023-03-04 21:59:00 -0800

    Added a reducer to the store

--- bible-quiz-client/src/redux/store.js
+++ bible-quiz-client/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import questionReducer from "./thousandQuestionsSlice";
+import revisionReducer from "./revisionQuestionSlice"
 
 const rootReducer = combineReducers({
   thousandQuestions: questionReducer,
+  revisionQuestions: revisionReducer
 });
 
 export default configureStore({ reducer: rootReducer });
