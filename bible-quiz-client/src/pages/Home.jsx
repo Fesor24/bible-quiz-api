64cd89bc43fcdd44434bb199c1683e14d4194654
Wale <onafesowale@gmail.com>
2023-03-04 21:58:37 -0800

    Modified styling

--- bible-quiz-client/src/pages/Home.jsx
+++ bible-quiz-client/src/pages/Home.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../components/Button';
 import style from '../styles/Home.module.css';
+import { useEffect } from 'react';
 
 function Home() {
+
+  useEffect(() => {
+    document.title = "Home"
+  }, [])
+
   return (
     <div className={style.container}>
       <h1>gtcc quiz preparation</h1>
