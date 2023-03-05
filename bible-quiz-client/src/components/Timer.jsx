962964a8d8812e7a507d7e1c4ddc00515e0bec38
Wale <onafesowale@gmail.com>
2023-03-04 21:57:59 -0800

    Remove timer when questions finish

--- bible-quiz-client/src/components/Timer.jsx
+++ bible-quiz-client/src/components/Timer.jsx
@@ -2,20 +2,11 @@ import style from "../styles/Timer.module.css";
 
 function Timer({countdown}) {
 
-  
-  // const borderWidth = {
-  //     border: "4px solid brown"
-  // }
-
-  // const colorText = {
-  //     color: "brown"
-  // }
-
   return (
     <div className={style.timer}>
       {/* <Circle borderWidth ={borderWidth} circleText= {"00:30"} colorText={colorText} /> */}
 
-      <p>00:{countdown === 0 ? "00": countdown}</p>
+      <p>00:{countdown === 0 ? "00": countdown < 10 ? `0${countdown}` : countdown}</p>
     </div>
   );
 }
