bcf06be8879056cf85d6e82a5432c3b999312414
Wale <onafesowale@gmail.com>
2023-03-12 08:03:53 -0700

    redux store

--- bible-quiz-client/src/redux/store.js
+++ bible-quiz-client/src/redux/store.js
@@ -1,10 +1,14 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import questionReducer from "./thousandQuestionsSlice";
-import revisionReducer from "./revisionQuestionSlice"
+import revisionReducer from "./revisionQuestionSlice";
+import globalReducer from "./globalSlice"
+import fesorReducer from "./fesorQuestionsSlice";
 
 const rootReducer = combineReducers({
   thousandQuestions: questionReducer,
-  revisionQuestions: revisionReducer
+  revisionQuestions: revisionReducer,
+  globalSetting: globalReducer,
+  fesorQuestions: fesorReducer
 });
 
 export default configureStore({ reducer: rootReducer });
