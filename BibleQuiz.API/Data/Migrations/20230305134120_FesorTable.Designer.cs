134178fd87ad3913606de07008e0488c7886c914
Wale <onafesowale@gmail.com>
2023-03-12 08:01:30 -0700

    Migration files

--- BibleQuiz.API/Data/Migrations/20230305134120_FesorTable.Designer.cs
+++ BibleQuiz.API/Data/Migrations/20230305134120_FesorTable.Designer.cs
@@ -4,6 +4,7 @@ using BibleQuiz.Core;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BibleQuiz.API.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230305134120_FesorTable")]
+    partial class FesorTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
