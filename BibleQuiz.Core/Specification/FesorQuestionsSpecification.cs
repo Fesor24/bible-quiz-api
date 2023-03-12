a59a43ff86ec1b184b3eed7a05e0bd4afb702e52
Wale <onafesowale@gmail.com>
2023-03-12 07:59:59 -0700

    Fesor questions specification

---
+++ BibleQuiz.Core/Specification/FesorQuestionsSpecification.cs
@@ -0,0 +1,10 @@
+﻿namespace BibleQuiz.Core
+{
+	public class FesorQuestionsSpecification: BaseSpecification<FesorQuestionsDataModel>
+	{
+		public FesorQuestionsSpecification()
+		{
+			AddOrderBy(x => x.Id);
+		}
+	}
+}
