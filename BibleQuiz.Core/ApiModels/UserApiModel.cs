17dd5673e6c2839b82c8bdf8f0f3d2bc71930d44
Wale <onafesowale@gmail.com>
2023-03-25 07:20:17 -0700

    User api model

---
+++ BibleQuiz.Core/ApiModels/UserApiModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BibleQuiz.Core
+{
+	public class UserApiModel
+	{
+		public string Email { get; set; }
+
+		public string Token { get; set; }
+
+		public Permission Permission { get; set; }
+	}
+}
