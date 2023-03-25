24e40bf70e9253b388c4eb697113a1a7be35400b
Wale <onafesowale@gmail.com>
2023-03-25 07:20:03 -0700

    Added action in global slice

--- bible-quiz-client/src/redux/globalSlice.js
+++ bible-quiz-client/src/redux/globalSlice.js
@@ -1,9 +1,12 @@
 import {createSlice} from "@reduxjs/toolkit"
 
 const globalSlice = createSlice({
-    name: "global",
+    name: 'global',
     initialState: {
-        countdown: 0
+        countdown: 0,
+        isAuthenticated: false,
+        hasAccess: false
+
     },
     reducers:{
         changeCountdownAction: (state, payload) => {
@@ -11,9 +14,22 @@ const globalSlice = createSlice({
                 ...state,
                 countdown: state.countdown = payload
             }
+        },
+        isAuthenticatedAction: (state, payload) => {
+            return{
+                ...state,
+                isAuthenticated: state.isAuthenticated = payload
+            }
+        },
+        hasAccessAction: (state, payload) =>{
+            return{
+                ...state,
+                hasAccess: state.hasAccess = payload
+            }
         }
     }
 })
 
-export const {changeCountdownAction} = globalSlice.actions;
+export const { changeCountdownAction, isAuthenticatedAction, hasAccessAction } =
+  globalSlice.actions;
 export default globalSlice.reducer
