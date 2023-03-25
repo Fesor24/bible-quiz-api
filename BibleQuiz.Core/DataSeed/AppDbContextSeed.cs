5fbfdec8cee101c85837766ce3840857bf11f41c
Wale <onafesowale@gmail.com>
2023-03-25 07:20:37 -0700

    Seeded user with claims and permission

--- BibleQuiz.Core/DataSeed/AppDbContextSeed.cs
+++ BibleQuiz.Core/DataSeed/AppDbContextSeed.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.Json;
+﻿using System.Security.Claims;
+using System.Text.Json;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
 
 namespace BibleQuiz.Core
@@ -59,6 +61,106 @@ namespace BibleQuiz.Core
 
                 // Log error to console
                 logger.LogError("An error occurred", ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// To seed user to db
+        /// </summary>
+        /// <param name="userManager"></param>
+        /// <param name="loggerFactory"></param>
+        /// <returns></returns>
+        public static async Task SeedUserAsync(UserManager<ApplicationUser> userManager,ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                // If we have no user in db
+                if (!userManager.Users.Any())
+                {
+                    // Create the user
+                    var result = await userManager.CreateAsync(new ApplicationUser
+                    {
+                        Email = "fesor@gtcc.admin.com",
+                        UserName = "fesor@gtcc.admin.com",
+                        FirstName = "Fesor",
+                        LastName = "Dev",
+                        Permission = Permission.Granted
+                    }, "Passw0rd_123");
+
+                    if (result.Succeeded)
+                    {
+                        // Find the user by mail
+                        var user = await userManager.FindByEmailAsync("fesor@gtcc.admin.com");
+
+                        // Add claimes to the user
+                        await userManager.AddClaimsAsync(user, new List<Claim>
+                        {
+                            new Claim(ClaimTypes.Role, "Admin"),
+                            new Claim("premiumuser", "PremiumUser")
+                        });
+
+                        // Giving the user admin role
+                        //await userManager.AddToRoleAsync(user, "Admin");
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                // Create a logger of tyoe AppDbContext
+                var logger = loggerFactory.CreateLogger(typeof(AppDbContextSeed));
+
+                // Log error to console
+                logger.LogError("An error ocurred. Details: {error}", ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Seed roles in the db
+        /// </summary>
+        /// <param name="roleManager"></param>
+        /// <param name="loggerFactory"></param>
+        /// <returns></returns>
+        public static async Task SeedRolesAsync(RoleManager<IdentityRole> roleManager, ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                // Check if the admin role exist
+                if (!await roleManager.RoleExistsAsync("Admin"))
+                {
+                    // Create an instance of identity role
+                    var newRole = new IdentityRole("Admin");
+
+                    // Add the role to the table
+                    await roleManager.CreateAsync(newRole);
+                }
+
+                // Check if premium user role exists
+                if(!await roleManager.RoleExistsAsync("PremiumUser"))
+                {
+                    // Create it if it doesn't
+                    await roleManager.CreateAsync(new IdentityRole
+                    {
+                        Name = "PremiumUser"
+                    });
+                }
+
+                // Check if user role exist
+                if (!await roleManager.RoleExistsAsync("User"))
+                {
+                    // Create it if it does not
+                    await roleManager.CreateAsync(new IdentityRole
+                    {
+                        Name = "User"
+                    });
+                }
+            }
+            catch(Exception ex)
+            {
+                // Create a logger of type app db context seed
+                var logger = loggerFactory.CreateLogger(typeof(AppDbContextSeed));
+
+                // Log the error to console
+                logger.LogError("An erro occurred. Details: {error}", ex.Message);
             }
         }
     }
