433e143a03bc278784c7495c1ed7ee65151499da
Wale <onafesowale@gmail.com>
2023-03-25 07:23:43 -0700

    Account credentials api model

---
+++ BibleQuiz.Core/ApiModels/LoginCredentialsApiModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BibleQuiz.Core
+{
+    public class LoginCredentialsApiModel
+    {
+		[Required(ErrorMessage = "Email is a required field")]
+		[EmailAddress]
+		public string Email { get; set; }
+
+		/// <summary>
+		/// The password of the user
+		/// </summary>
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+	}
+}
---
+++ BibleQuiz.Core/ApiModels/RegisterCredentialsApiModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BibleQuiz.Core
+{
+	public class RegisterCredentialsApiModel
+	{
+		/// <summary>
+		/// The first name of the user
+		/// </summary>
+		[Required(ErrorMessage = "First name is a required field")]
+		public string FirstName { get; set; }
+
+		/// <summary>
+		/// The last name of the user
+		/// </summary>
+		[Required(ErrorMessage = "Last name is a required field")]
+		public string LastName { get; set; }
+
+		/// <summary>
+		/// The email of the user
+		/// </summary>
+		[EmailAddress]
+		[Required(ErrorMessage = "Email is a required field")]
+		public string Email { get; set; }
+
+		/// <summary>
+		/// The password of the user
+		/// </summary>
+		[DataType(DataType.Password)]
+		[Required(ErrorMessage = "Password is a required field")]
+		public string Password { get; set; }
+	}
+}
