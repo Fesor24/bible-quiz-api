83786f7361ab28b3a26ae1140511af3b250e7aed
Wale <onafesowale@gmail.com>
2023-03-27 22:38:01 -0700

    grant access api model

--- BibleQuiz.Core/ApiModels/GrantAccessApiModel.cs
+++ BibleQuiz.Core/ApiModels/GrantAccessApiModel.cs
@@ -1,7 +1,13 @@
 ﻿namespace BibleQuiz.Core
 {
+	/// <summary>
+	/// Api model to grant acces to third parties
+	/// </summary>
 	public class GrantAccessApiModel
 	{
+		/// <summary>
+		/// The email of the third party
+		/// </summary>
 		public string Email { get;set; }
 	}
 }
