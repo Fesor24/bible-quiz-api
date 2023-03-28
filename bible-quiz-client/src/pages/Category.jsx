23ca29ad8fd23ce258d397e01af7a053aa12eac0
Wale <onafesowale@gmail.com>
2023-03-27 22:40:08 -0700

    Dynamic buttons for login and logout

--- bible-quiz-client/src/pages/Category.jsx
+++ bible-quiz-client/src/pages/Category.jsx
@@ -1,18 +1,33 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import Button from "../components/Button";
 import style from "../styles/Home.module.css";
 import { Link } from "react-router-dom";
+import { checkTokenForExpiry } from "../helper/coreFunction";
 
 function Category() {
 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   useEffect(() => {
     document.title = "Category";
-  }, [])
+
+    const loggedIn = checkTokenForExpiry();
+
+    setIsLoggedIn(loggedIn);
+
+    
+  }, [isLoggedIn]);
+
+  const handleLogOut = () =>{
+    setIsLoggedIn(false);
+    localStorage.removeItem("token");
+    localStorage.removeItem("hasAccess");
+  }
 
   return (
     <div className={style.container}>
       <div className={style.mainWrapper}>
-        <h1 className={style.mainTitle}>Select a category</h1>
+        <h1 className={style.mainTitle}>Sections</h1>
         <Link to="/thousand-questions">
           <Button name="1000 Questions">
             <i class="fa fa-file-text" aria-hidden="true"></i>
@@ -31,11 +46,23 @@ function Category() {
           </Button>
         </Link>
 
-        <Link to="/login">
-          <Button name="Login" padding={'.7rem 2rem'}>
-            <i class="fa-solid fa-right-to-bracket"></i>
-          </Button>
-        </Link>
+        {isLoggedIn ? (
+          <>
+            <Link to="/category">
+              <Button name="Logout" click={handleLogOut}>
+                <i class="fa-solid fa-arrow-right-from-bracket"></i>
+              </Button>
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link to="/login">
+              <Button name="Login">
+                <i class="fa-solid fa-right-from-bracket"></i>
+              </Button>
+            </Link>
+          </>
+        )}
 
         <Link to="/" className={style.link}>
           Back to home
