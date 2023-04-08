f5552aefcf3173f9aaf4e4d51544786ba35647a3
Wale <onafesowale@gmail.com>
2023-04-08 05:15:32 -0700

    Api model to add questions

---
+++ BibleQuiz.Core/ApiModels/CreateQuestionsApiModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BibleQuiz.Core
+{
+	public class CreateQuestionsApiModel
+	{
+		/// <summary>
+		/// The question
+		/// </summary>
+		[Required]
+		public string Question { get; set; }
+
+		/// <summary>
+		/// The answer to this questions
+		/// </summary>
+		[Required]
+		public string Answer { get; set; }
+	}
+}
