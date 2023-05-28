cda263721f3ae0b37d1714ddbc6465387fc7f860
Wale <onafesowale@gmail.com>
2023-05-28 07:50:47 -0700

    VerseOfTheDay DataModel

---
+++ BibleQuiz.Core/DataModels/VerseOfTheDayDataModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BibleQuiz.Core
+{
+    public class VerseOfTheDayDataModel
+    {
+        public int Id { get; set; }
+
+        public string Book { get; set; }
+
+        public string Passage { get; set; }
+    }
+}
