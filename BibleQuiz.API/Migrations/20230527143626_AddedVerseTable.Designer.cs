4d9b850540224641e7cec93f2fa95c9ec9285c00
Wale <onafesowale@gmail.com>
2023-05-28 07:48:44 -0700

    Migration files

--- BibleQuiz.API/Migrations/20230527143626_AddedVerseTable.Designer.cs
+++ BibleQuiz.API/Migrations/20230527143626_AddedVerseTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BibleQuiz.Core;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BibleQuiz.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230527143626_AddedVerseTable")]
+    partial class AddedVerseTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -169,7 +171,7 @@ namespace BibleQuiz.API.Migrations
                     b.Property<string>("Book")
                         .HasColumnType("text");
 
-                    b.Property<string>("Passage")
+                    b.Property<string>("Pasage")
                         .HasColumnType("text");
 
                     b.HasKey("Id");
--- BibleQuiz.API/Migrations/20230527144259_RenamedProp.Designer.cs
+++ BibleQuiz.API/Migrations/20230527144259_RenamedProp.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BibleQuiz.Core;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BibleQuiz.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230527144259_RenamedProp")]
+    partial class RenamedProp
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
