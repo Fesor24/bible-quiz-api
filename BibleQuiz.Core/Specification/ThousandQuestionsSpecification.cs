47263207c0576ec5580142772651cc6e14330a91
Wale <onafesowale@gmail.com>
2023-05-28 07:49:35 -0700

    removed unused namespaces

--- BibleQuiz.Core/Specification/ThousandQuestionsSpecification.cs
+++ BibleQuiz.Core/Specification/ThousandQuestionsSpecification.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BibleQuiz.Core
+﻿namespace BibleQuiz.Core
 {
     public class ThousandQuestionsSpecification: BaseSpecification<ThousandQuizQuestionsDataModel>
     {
