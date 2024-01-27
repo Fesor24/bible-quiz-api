5a409b8b378049d8c215e3231d2306b41c13761d
Wale <onafesowale@gmail.com>
2024-01-27 18:37:01 +0100

    IGeneric repository

---
+++ BibleQuiz.Domain/Primitives/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace BibleQuiz.Domain.Primitives;
+public interface IGenericRepository<TEntity> where TEntity : Entity
+{
+    Task AddAsync(TEntity entity);
+    Task AddRangeAsync(IEnumerable<TEntity> entities);
+    void Update(TEntity entity);
+    void Delete(TEntity entity);
+    Task<List<TEntity>> GetAllAsync();
+    Task<TEntity> GetAsync(ISpecification<TEntity> spec);
+    Task<List<TEntity>> GetAllWithSpecAsync(ISpecification<TEntity> spec);
+
+}
