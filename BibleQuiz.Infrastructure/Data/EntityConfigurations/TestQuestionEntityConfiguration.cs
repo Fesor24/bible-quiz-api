788cc5e8b4da3d70e289ebd095826c28bc4647b5
Wale <onafesowale@gmail.com>
2024-01-27 18:38:47 +0100

    Test question entity configuration

---
+++ BibleQuiz.Infrastructure/Data/EntityConfigurations/TestQuestionEntityConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using BibleQuiz.Domain.Entities;
+using BibleQuiz.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BibleQuiz.Infrastructure.Data.EntityConfigurations;
+internal class TestQuestionEntityConfiguration : IEntityTypeConfiguration<TestQuestion>
+{
+    public void Configure(EntityTypeBuilder<TestQuestion> builder)
+    {
+        builder.ToTable(nameof(TestQuestion), QuizDbContext.COMMON);
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Source)
+            .HasConversion(sr => sr.ToString(),
+            c => (QuestionSource)Enum.Parse(typeof(QuestionSource), c));
+    }
+}
