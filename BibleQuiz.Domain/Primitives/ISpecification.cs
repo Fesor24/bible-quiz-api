39e08e872289c290609d6f5a88fd99b03d43fa64
Wale <onafesowale@gmail.com>
2024-01-27 18:37:08 +0100

    ISpecification

---
+++ BibleQuiz.Domain/Primitives/ISpecification.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq.Expressions;
+
+namespace BibleQuiz.Domain.Primitives;
+public interface ISpecification<TEntity> where TEntity : Entity
+{
+    Expression<Func<TEntity, bool>> Criteria { get; }
+    Expression<Func<TEntity, object>> OrderByDesc { get; }
+}
