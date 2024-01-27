73cbc38b8dc4bdcd8529e0a7c626f4affc7b2280
Wale <onafesowale@gmail.com>
2024-01-27 18:39:09 +0100

    quiz db context

---
+++ BibleQuiz.Infrastructure/Data/QuizDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BibleQuiz.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BibleQuiz.Infrastructure.Data;
+public class QuizDbContext : IdentityDbContext<User>
+{
+    internal const string USER = "usr";
+    internal const string COMMON = "com";
+
+    public DbSet<TestQuestion> TestQuestions => Set<TestQuestion>();
+    public DbSet<Verse> Verse => Set<Verse>();
+}
