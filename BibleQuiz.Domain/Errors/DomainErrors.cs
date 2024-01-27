da26ab132a9c4a6df176302889256f5b92722a22
Wale <onafesowale@gmail.com>
2024-01-27 18:36:29 +0100

    domain errors

---
+++ BibleQuiz.Domain/Errors/DomainErrors.cs
@@ -0,0 +1,18 @@
+﻿using BibleQuiz.Domain.Shared;
+
+namespace BibleQuiz.Domain.Errors;
+internal static class DomainErrors
+{
+    internal static class TestQuestion
+    {
+        internal static Error InvalidQuestion = new("400", "Invalid question");
+
+        internal static Error InvalidAnswer = new("400", "Invalid answer");
+    }
+
+    internal static class Verse
+    {
+        internal static Error InvalidTitle = new("400", "Invalid title");
+        internal static Error InvalidPassage = new("400", "Invalid passage");
+    }
+}
