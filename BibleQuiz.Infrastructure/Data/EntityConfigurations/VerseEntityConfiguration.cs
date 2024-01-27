ea43b2ca45c4c3922dbdd1d74b2cbafd024a2ef6
Wale <onafesowale@gmail.com>
2024-01-27 18:39:02 +0100

    verse entity configuration

---
+++ BibleQuiz.Infrastructure/Data/EntityConfigurations/VerseEntityConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using BibleQuiz.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BibleQuiz.Infrastructure.Data.EntityConfigurations;
+internal class VerseEntityConfiguration : IEntityTypeConfiguration<Verse>
+{
+    public void Configure(EntityTypeBuilder<Verse> builder)
+    {
+        builder.ToTable(nameof(Verse), QuizDbContext.COMMON);
+        builder.HasKey(x => x.Id);
+    }
+}
