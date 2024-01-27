4e9f18b6a812bb6de25bb645f77f6d90b06d2816
Wale <onafesowale@gmail.com>
2024-01-27 18:37:31 +0100

    Result

---
+++ BibleQuiz.Domain/Shared/Result.cs
@@ -0,0 +1,24 @@
+﻿namespace BibleQuiz.Domain.Shared;
+public class Result
+{
+    protected internal Result(bool isSucccess, Error error)
+    {
+        IsSuccess = isSucccess;
+        Error = error;
+    }
+
+    protected internal Result()
+    {
+        IsSuccess = true;
+        Error = Error.None;
+    }
+
+    public bool IsSuccess { get; set; }
+    public bool IsFailure => !IsSuccess;
+    public Error Error { get;}
+
+    public static Result Success => new(true, Error.None);
+    public static Result Failure(Error error) => new(false, error);
+
+
+}
