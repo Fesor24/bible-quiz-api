48cc58216ee897d804b96f2acea101e898f257f7
Wale <onafesowale@gmail.com>
2024-01-27 18:39:37 +0100

    specification evaluator

---
+++ BibleQuiz.Infrastructure/Specifications/SpecificationEvaluator.cs
@@ -0,0 +1,17 @@
+﻿using BibleQuiz.Domain.Primitives;
+
+namespace BibleQuiz.Infrastructure.Specifications;
+internal static class SpecificationEvaluator
+{
+    internal static IQueryable<TEntity> GetQuery<TEntity>(ISpecification<TEntity> spec, IQueryable<TEntity> query) 
+        where TEntity: Entity
+    {
+        if (spec.Criteria is not null)
+            query = query.Where(spec.Criteria);
+
+        if (spec.OrderByDesc is not null)
+            query = query.OrderByDescending(spec.OrderByDesc);
+
+        return query;
+    }
+}
