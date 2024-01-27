26463571963c89e18de09d0be71494521b898b7c
Wale <onafesowale@gmail.com>
2024-01-27 18:39:27 +0100

    base specification

---
+++ BibleQuiz.Infrastructure/Specifications/BaseSpecification.cs
@@ -0,0 +1,23 @@
+﻿using BibleQuiz.Domain.Primitives;
+using System.Linq.Expressions;
+
+namespace BibleQuiz.Infrastructure.Specifications;
+public class BaseSpecification<TEntity> : ISpecification<TEntity> where TEntity: Entity
+{
+    public BaseSpecification()
+    {
+        
+    }
+
+    public BaseSpecification(Expression<Func<TEntity, bool>> criteria)
+    {
+        Criteria = criteria;
+    }
+
+    public Expression<Func<TEntity, bool>> Criteria { get; private set; }
+
+    public Expression<Func<TEntity, object>> OrderByDesc { get; private set; }
+
+    protected void SetOrderByDesc(Expression<Func<TEntity, object>> orderByDesc) =>
+        OrderByDesc = orderByDesc;
+}
