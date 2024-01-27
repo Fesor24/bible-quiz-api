58ee66937da398ab930be4bb5af756ec1ee07022
Wale <onafesowale@gmail.com>
2024-01-27 18:36:21 +0100

    Question source enum

---
+++ BibleQuiz.Domain/Enums/QuestionSource.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace BibleQuiz.Domain.Enums;
+public enum QuestionSource
+{
+    [EnumMember(Value ="Unknown")]
+    Unknown,
+    [EnumMember(Value ="BibleQuizzes")]
+    BibleQuizzes,
+    [EnumMember(Value = "Author")]
+    Author
+}
