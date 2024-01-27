91fbec0a19669f1e21bbb71ef3bf6df48e457dea
Wale <onafesowale@gmail.com>
2024-01-27 18:37:24 +0100

    Error

---
+++ BibleQuiz.Domain/Shared/Error.cs
@@ -0,0 +1,16 @@
+﻿namespace BibleQuiz.Domain.Shared;
+public class Error
+{
+    public Error(string code, string message)
+    {
+        Code = code;
+        Message = message;
+    }
+
+    public string Code { get; set; }
+    public string Message { get; set; }
+
+    public static readonly Error None = new(string.Empty, string.Empty);
+
+    public static implicit operator Result(Error error) => Result.Failure(error);
+}
