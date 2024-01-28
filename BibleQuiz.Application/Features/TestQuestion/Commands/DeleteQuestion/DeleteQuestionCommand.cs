a18f6b227461b054ab1467c33b190f83d81a979a
Wale <onafesowale@gmail.com>
2024-01-28 06:57:50 +0100

    delete question command

---
+++ BibleQuiz.Application/Features/TestQuestion/Commands/DeleteQuestion/DeleteQuestionCommand.cs
@@ -0,0 +1,8 @@
+﻿using BibleQuiz.Domain.Shared;
+using MediatR;
+
+namespace BibleQuiz.Application.Features.TestQuestion.Commands.DeleteQuestion;
+public class DeleteQuestionCommand : IRequest<Result<Unit, Error>>
+{
+    public int Id { get; set; }
+}
