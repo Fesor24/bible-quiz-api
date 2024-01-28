8bb430ac0a75530ada970a2c718b4bd130bb36aa
Wale <onafesowale@gmail.com>
2024-01-28 07:12:20 +0100

    controller extensions

---
+++ BibleQuiz.API/Extensions/ControllerExtensions.cs
@@ -0,0 +1,15 @@
+﻿using BibleQuiz.Domain.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BibleQuiz.API.Extensions;
+
+public static class ControllerExtensions
+{
+    public static IActionResult HandleErrorResult(this ControllerBase controller, Error error)
+    {
+        if (error.Code == "404")
+            return controller.NotFound(error);
+        else
+            return controller.BadRequest(error);
+    }
+}
