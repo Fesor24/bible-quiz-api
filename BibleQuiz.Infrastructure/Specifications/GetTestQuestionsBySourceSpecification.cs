f09d1c5a61c37937baf7f354642837af52f324c0
Wale <onafesowale@gmail.com>
2024-01-28 07:00:39 +0100

    specification

---
+++ BibleQuiz.Infrastructure/Specifications/GetTestQuestionsBySourceSpecification.cs
@@ -0,0 +1,10 @@
+﻿using BibleQuiz.Domain.Entities;
+using BibleQuiz.Domain.Enums;
+
+namespace BibleQuiz.Infrastructure.Specifications;
+public class GetTestQuestionsBySourceSpecification : BaseSpecification<TestQuestion>
+{
+    public GetTestQuestionsBySourceSpecification(QuestionSource source) : base(x => x.Source == source)
+    {
+    }
+}
