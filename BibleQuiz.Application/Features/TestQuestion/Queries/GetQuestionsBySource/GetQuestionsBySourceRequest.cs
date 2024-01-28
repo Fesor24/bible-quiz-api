e86896ad44bce93db391b1f4c01c1f89aac8cf4b
Wale <onafesowale@gmail.com>
2024-01-28 06:58:26 +0100

    get questions by source request

---
+++ BibleQuiz.Application/Features/TestQuestion/Queries/GetQuestionsBySource/GetQuestionsBySourceRequest.cs
@@ -0,0 +1,10 @@
+﻿using BibleQuiz.Application.Features.TestQuestion.Queries.GetQuestions;
+using BibleQuiz.Domain.Enums;
+using BibleQuiz.Domain.Shared;
+using MediatR;
+
+namespace BibleQuiz.Application.Features.TestQuestion.Queries.GetQuestionsBySource;
+public class GetQuestionsBySourceRequest : IRequest<Result<IReadOnlyList<GetQuestionResponse>, Error>>
+{
+    public QuestionSource Source { get; set; }
+}
