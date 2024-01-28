633dc4fac0dd07d6f39979ef435be8de448a2c3d
Wale <onafesowale@gmail.com>
2024-01-28 07:00:19 +0100

    unit of work

---
+++ BibleQuiz.Infrastructure/Repository/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using BibleQuiz.Domain.Primitives;
+using BibleQuiz.Infrastructure.Data;
+using System.Collections;
+
+namespace BibleQuiz.Infrastructure.Repository;
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly QuizDbContext _context;
+    private Hashtable _repos;
+
+    public UnitOfWork(QuizDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> Complete() => 
+        await _context.SaveChangesAsync();
+
+    public void Dispose() =>
+        _context.Dispose();
+
+    public IGenericRepository<TEntity> Repository<TEntity>() where TEntity : Entity
+    {
+        _repos ??= new Hashtable();
+
+        var key = typeof(TEntity).FullName;
+
+        if (!_repos.ContainsKey(key))
+        {
+            var genericType = typeof(GenericRepository<>);
+
+            var repositoryInstance = Activator.CreateInstance(
+                genericType.MakeGenericType(typeof(TEntity)), _context);
+
+            _repos.Add(key, repositoryInstance);
+        }
+
+        return (IGenericRepository<TEntity>) _repos[key];
+    }
+}
