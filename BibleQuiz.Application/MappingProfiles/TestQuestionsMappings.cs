bab6c029bf7142eb9eed67e90fcc49a1286e22ce
Wale <onafesowale@gmail.com>
2024-01-28 06:59:12 +0100

    test questions mapping

---
+++ BibleQuiz.Application/MappingProfiles/TestQuestionsMappings.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BibleQuiz.Application.Features.TestQuestion.Commands.CreateQuestion;
+using BibleQuiz.Application.Features.TestQuestion.Queries.GetQuestions;
+using BibleQuiz.Domain.Entities;
+
+namespace BibleQuiz.Application.MappingProfiles;
+public sealed class TestQuestionsMappings : Profile
+{
+    public TestQuestionsMappings()
+    {
+        CreateMap<TestQuestion, GetQuestionResponse>();
+
+        CreateMap<CreateQuestionsDto, TestQuestion>();
+    }
+}
