a4695f83d8fa0eddc9a609bbdbb8df843fb4aa3f
Wale <onafesowale@gmail.com>
2024-01-28 07:00:09 +0100

    dependency injection

---
+++ BibleQuiz.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using BibleQuiz.Domain.Primitives;
+using BibleQuiz.Infrastructure.Data;
+using BibleQuiz.Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BibleQuiz.Infrastructure;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        services.AddDbContext<QuizDbContext>(opt =>
+        {
+            opt.UseNpgsql(config.GetConnectionString("DefaultConnection"),
+                migrations => migrations.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName));
+        });
+
+        return services;
+    }
+}
