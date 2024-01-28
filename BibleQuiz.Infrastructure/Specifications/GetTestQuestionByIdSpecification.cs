8e6c864b9809d6ceaf3949056ebc18cdc27801c4
Wale <onafesowale@gmail.com>
2024-01-28 07:00:32 +0100

    specification

---
+++ BibleQuiz.Infrastructure/Specifications/GetTestQuestionByIdSpecification.cs
@@ -0,0 +1,10 @@
+﻿using BibleQuiz.Domain.Entities;
+
+namespace BibleQuiz.Infrastructure.Specifications;
+public sealed class GetTestQuestionByIdSpecification : BaseSpecification<TestQuestion>
+{
+    public GetTestQuestionByIdSpecification(int id) : base(x => x.Id == id)
+    {
+        
+    }
+}
