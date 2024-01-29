1819e358631050cf69ffe173a20b0e118443f200
Wale <onafesowale@gmail.com>
2024-01-29 04:15:12 +0100

    comment code

--- BibleQuiz.API/Extensions/ApplicationServiceExtensions.cs
+++ BibleQuiz.API/Extensions/ApplicationServiceExtensions.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using AspNetCoreRateLimit;
 using BibleQuiz.Core;
+using BibleQuiz.Infrastructure.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -17,86 +18,62 @@ namespace BibleQuiz.API
         /// </summary>
         /// <param name="app"></param>
         /// <returns></returns>
-        public static async Task ApplyMigrationsAsync(this WebApplication app)
-        {
-            using (var scope = app.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
+        //public static async Task ApplyMigrationsAsync(this WebApplication app)
+        //{
+        //    using (var scope = app.Services.CreateScope())
+        //    {
+        //        var services = scope.ServiceProvider;
 
-                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+        //        var loggerFactory = services.GetRequiredService<ILoggerFactory>();
 
-                try
-                {
-                    // Getting the required service for AppDbContext
-                    var context = services.GetRequiredService<ApplicationDbContext>();
+        //        try
+        //        {     
+        //            var context = services.GetRequiredService<QuizDbContext>();
 
-                    // Get service for user manager
-                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+        //            // Get service for user manager
+        //            //var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
 
-                    // Get service for role manager
-                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+        //            //// Get service for role manager
+        //            //var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
 
-                    // Apply pending migrations to db
-                    await context.Database.MigrateAsync();
+        //            // Apply pending migrations to db
+        //            await context.Database.MigrateAsync();
 
-                    // Seed questions to db
-                    await AppDbContextSeed.SeedDataAsync(context, loggerFactory);
+        //            // Seed questions to db
+        //            //await AppDbContextSeed.SeedDataAsync(context, loggerFactory);
 
-                    // Seed roles to db
-                    await AppDbContextSeed.SeedRolesAsync(roleManager, loggerFactory);
+        //            //// Seed roles to db
+        //            //await AppDbContextSeed.SeedRolesAsync(roleManager, loggerFactory);
 
-                    // Seed the user to db
-                    await AppDbContextSeed.SeedUserAsync(userManager, loggerFactory);
+        //            //// Seed the user to db
+        //            //await AppDbContextSeed.SeedUserAsync(userManager, loggerFactory);
 
-                }
+        //        }
 
-                catch(Exception ex)
-                {
-                    // Create logger
-                    var logger = loggerFactory.CreateLogger<Program>();
+        //        catch(Exception ex)
+        //        {
+        //            // Create logger
+        //            var logger = loggerFactory.CreateLogger<Program>();
 
-                    // Log error to console
-                    logger.LogError($"An error occurred while applying migrations. Details: {ex.Message + ex.StackTrace}");
-                }
-            }
-        }
+        //            // Log error to console
+        //            logger.LogError($"An error occurred while applying migrations. Details: {ex.Message + ex.StackTrace}");
+        //        }
+        //    }
+        //}
 
-        /// <summary>
-        /// Configure DbContext
-        /// </summary>
-        /// <param name="services"></param>
-        /// <param name="config"></param>
-        /// <returns></returns>
-        public static IServiceCollection AddDbContext(this IServiceCollection services, IConfiguration config)
-        {
-            services.AddDbContext<ApplicationDbContext>(options =>
-            {
-                options.UseNpgsql(config.GetConnectionString("DefaultConnection"),
-                    migrations => migrations.MigrationsAssembly(typeof(Program).Assembly.GetName().Name));
+        //public static IServiceCollection AddIdentity(this IServiceCollection services)
+        //{
+        //    services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+        //    {
+        //        options.Password.RequiredUniqueChars = 0;
+        //        options.Password.RequireNonAlphanumeric = false;
+        //        options.Password.RequireDigit = false;
+        //        options.Password.RequireUppercase = false;
+        //    }).AddEntityFrameworkStores<ApplicationDbContext>()
+        //        .AddDefaultTokenProviders();
 
-            });
-
-            return services;
-        }
-
-        /// <summary>
-        /// Configure AspNet Identity
-        /// </summary>
-        /// <param name="services"></param>
-        /// <returns></returns>
-        public static IServiceCollection AddIdentity(this IServiceCollection services)
-        {
-            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
-            {
-                options.Password.RequiredUniqueChars = 0;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireDigit = false;
-                options.Password.RequireUppercase = false;
-            }).AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
-
-            return services;
-        }
+        //    return services;
+        //}
 
         /// <summary>
         /// Configure api validation error response
@@ -127,33 +104,6 @@ namespace BibleQuiz.API
             return services;
         }
 
-        /// <summary>
-        /// Register generic repository as a scoped service
-        /// </summary>
-        /// <param name="services"></param>
-        /// <returns></returns>
-        public static IServiceCollection AddGenericRepository(this IServiceCollection services)
-        {
-            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-
-            // Return services for further chaining
-            return services;
-        }
-
-        /// <summary>
-        /// Register UnitOfWork as scoped service
-        /// </summary>
-        /// <param name="services"></param>
-        /// <returns></returns>
-        public static IServiceCollection AddUnitOfWork(this IServiceCollection services)
-        {
-            
-            services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-            // Return services for further chaining
-            return services;
-        }
-
         public static IServiceCollection ConfigureCors(this IServiceCollection services)
         {
             services.AddCors(options =>
@@ -166,55 +116,49 @@ namespace BibleQuiz.API
                 });
             });
 
-            // Return services for further chaining
             return services;
         }
 
-        /// <summary>
-        /// Extension method to configure authorization
-        /// </summary>
-        /// <param name="services"></param>
-        /// <returns></returns>
-        public static IServiceCollection ConfigureAuthorization(this IServiceCollection services)
-        {
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy("RequireAdminClaim", policy =>
-                {
-                    policy.RequireClaim(ClaimTypes.Role, "Admin");
-                });
+        //public static IServiceCollection ConfigureAuthorization(this IServiceCollection services)
+        //{
+        //    services.AddAuthorization(options =>
+        //    {
+        //        options.AddPolicy("RequireAdminClaim", policy =>
+        //        {
+        //            policy.RequireClaim(ClaimTypes.Role, "Admin");
+        //        });
 
-                options.AddPolicy("RequirePremiumClaim", policy =>
-                {
-                    policy.RequireClaim("premiumuser", "PremiumUser");
-                });
-            });
+        //        options.AddPolicy("RequirePremiumClaim", policy =>
+        //        {
+        //            policy.RequireClaim("premiumuser", "PremiumUser");
+        //        });
+        //    });
 
-            return services;
-        }
+        //    return services;
+        //}
 
-        public static IServiceCollection ConfigureJwtAuthentication(this IServiceCollection services, IConfiguration config)
-        {
-            services.AddAuthentication(options =>
-            {
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-                .AddJwtBearer(options =>
-                {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JwtSettings:Key"])),
-                        ValidIssuer = config["JwtSettings:Issuer"],
-                        ValidateIssuer = true,
-                        ValidateAudience = false
-                    };
-                });
+        //public static IServiceCollection ConfigureJwtAuthentication(this IServiceCollection services, IConfiguration config)
+        //{
+        //    services.AddAuthentication(options =>
+        //    {
+        //        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        //        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        //    })
+        //        .AddJwtBearer(options =>
+        //        {
+        //            options.TokenValidationParameters = new TokenValidationParameters
+        //            {
+        //                ValidateIssuerSigningKey = true,
+        //                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JwtSettings:Key"])),
+        //                ValidIssuer = config["JwtSettings:Issuer"],
+        //                ValidateIssuer = true,
+        //                ValidateAudience = false
+        //            };
+        //        });
 
-            // Return services for further chaining
-            return services;
-        }
+        //    // Return services for further chaining
+        //    return services;
+        //}
 
         /// <summary>
         /// Add token service to ServiceCollection
@@ -228,41 +172,6 @@ namespace BibleQuiz.API
             return services;
         }
 
-        /// <summary>
-        /// Configure the rate limit for all endpoints
-        /// </summary>
-        /// <param name="services"></param>
-        /// <returns></returns>
-        public static IServiceCollection ConfigureRateLimitOptions(this IServiceCollection services)
-        {
-            var rateLimitRules = new List<RateLimitRule>
-            {
-                new RateLimitRule
-                {
-                    Endpoint = "*",
-                    Limit = 3,
-                    Period = "5m"
-                }
-            };
-
-            // Configure the IpRateLimitOptions
-            services.Configure<IpRateLimitOptions>(options => options.GeneralRules = rateLimitRules);
-
-            // Add the IRateLimitCounterStore as a singleton
-            services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
-
-            // Add the IIpPolicyStore as a singleton
-            services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
-
-            // Add the IRateLimitConfiguration as a singleton
-            services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
-
-            // Add the processing strategy as a singleton
-            services.AddSingleton<IProcessingStrategy, AsyncKeyLockProcessingStrategy>();
-
-            // Return services for further chaining
-            return services;
-        }
 
         /// <summary>
         /// Add options to IOC container
@@ -279,16 +188,16 @@ namespace BibleQuiz.API
             return services;
         }
 
-        public static IServiceCollection AddBibleApi(this IServiceCollection services)
-        {
-            services.AddHttpClient();
+        //public static IServiceCollection AddBibleApi(this IServiceCollection services)
+        //{
+        //    services.AddHttpClient();
 
-            services.AddTransient<IApiClient, ApiClient>();
+        //    services.AddTransient<IApiClient, ApiClient>();
 
-            services.AddScoped<IBibleService, BibleService>();
+        //    services.AddScoped<IBibleService, BibleService>();
 
-            return services;
-        }
+        //    return services;
+        //}
 
     }
 }
