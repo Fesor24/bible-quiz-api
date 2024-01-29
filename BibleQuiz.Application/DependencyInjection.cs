84de4ca85cecf935eb0953d29c06847dd3f4d294
Wale <onafesowale@gmail.com>
2024-01-29 04:17:28 +0100

    dependency injection

---
+++ BibleQuiz.Application/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using BibleQuiz.Application.Services;
+using BibleQuiz.Domain.Models;
+using BibleQuiz.Domain.Services;
+using FluentValidation;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BibleQuiz.Application;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        services.AddHttpClient();
+
+        services.AddTransient<IHttpClient, CustomHttpClient>();
+
+        services.AddTransient<IBibleService, BibleService>();
+
+        return services;
+    }
+}
