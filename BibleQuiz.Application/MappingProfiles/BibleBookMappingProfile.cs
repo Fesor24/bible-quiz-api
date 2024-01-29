e1714da8c8a44dd6faa99b634ff8d01a1b354aa7
Wale <onafesowale@gmail.com>
2024-01-29 04:21:01 +0100

    bible book mapping profile

---
+++ BibleQuiz.Application/MappingProfiles/BibleBookMappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using BibleQuiz.Application.Features.BibleBooks.Query.GetBibleBooks;
+using BibleQuiz.Domain.Entities;
+
+namespace BibleQuiz.Application.MappingProfiles;
+public class BibleBookMappingProfile : Profile
+{
+    public BibleBookMappingProfile()
+    {
+        CreateMap<BibleBook, GetBibleBooksResponse>();
+    }
+}
