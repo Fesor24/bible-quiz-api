83236b9858055b07eec3a0c9a8ed362cd4356e6f
Wale <onafesowale@gmail.com>
2024-01-29 04:25:23 +0100

    Bible Book entity

---
+++ BibleQuiz.Domain/Entities/BibleBook.cs
@@ -0,0 +1,26 @@
+﻿using BibleQuiz.Domain.Primitives;
+
+namespace BibleQuiz.Domain.Entities;
+public sealed class BibleBook : Entity
+{
+    public string BibleId { get; private set; }
+
+    public string ShortName { get; private set; }
+
+    public string NormalizedShortName { get; private set; }
+
+    public string LongName { get; private set; }
+
+    public BibleBook()
+    {
+            
+    }
+
+    public BibleBook(string bibleId, string shortName, string normalizedShortName, string longName)
+    {
+        BibleId = bibleId;
+        ShortName = shortName;
+        NormalizedShortName = normalizedShortName;
+        LongName = longName;
+    }
+}
