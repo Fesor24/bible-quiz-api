b55a51235d69cb1d709ec4cf104e6e14c8b368f6
Wale <onafesowale@gmail.com>
2024-01-29 04:28:13 +0100

    add svc to svc collection

--- BibleQuiz.Infrastructure/DependencyInjection.cs
+++ BibleQuiz.Infrastructure/DependencyInjection.cs
@@ -11,6 +11,8 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration config)
     {
+        services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
         services.AddScoped<IUnitOfWork, UnitOfWork>();
 
         services.AddDbContext<QuizDbContext>(opt =>
