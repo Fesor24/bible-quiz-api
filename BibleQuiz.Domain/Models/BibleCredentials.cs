d50392420e18ad948b3096cdd5c646b7fe126408
Wale <onafesowale@gmail.com>
2024-01-29 04:25:54 +0100

    Bible credentials

---
+++ BibleQuiz.Domain/Models/BibleCredentials.cs
@@ -0,0 +1,8 @@
+﻿namespace BibleQuiz.Domain.Models;
+public class BibleCredentials
+{
+    public const string CONFIGURATION = "BibleApi";
+    public string ApiKey { get; set; }
+    public string Url { get; set; }
+    public string KjvSpec { get; set; }
+}
