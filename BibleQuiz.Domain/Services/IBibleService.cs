c13b745403653e1d83ad92762a62f1834b007cc8
Wale <onafesowale@gmail.com>
2024-01-29 04:26:30 +0100

    IBible Service

---
+++ BibleQuiz.Domain/Services/IBibleService.cs
@@ -0,0 +1,10 @@
+﻿using BibleQuiz.Domain.Models;
+using BibleQuiz.Domain.Shared;
+
+namespace BibleQuiz.Domain.Services;
+public interface IBibleService
+{
+    Task<Result<BibleVerseSearchResponse, Error>> GetScripturesAsync(BibleVerse model);
+
+    Task<Result<BibleBooks, Error>> GetBibleBooks();
+}
