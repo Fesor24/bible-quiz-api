79cbad5cfd9e4afe52feef9205fb10aab220113d
Wale <onafesowale@gmail.com>
2024-01-29 04:26:18 +0100

    IHttp client

---
+++ BibleQuiz.Domain/Services/IHttpClient.cs
@@ -0,0 +1,9 @@
+﻿using BibleQuiz.Domain.Models;
+using BibleQuiz.Domain.Shared;
+
+namespace BibleQuiz.Domain.Services;
+public interface IHttpClient
+{
+    Task<Result<TResult, TError>> SendAsync<TBody, TResult, TError>
+            (HttpRequest<TBody> requestBody) where TError: Error;
+}
