a4ac6333cee3ea06a772fa8571ef7c3fe7c46684
Wale <onafesowale@gmail.com>
2024-01-29 04:18:10 +0100

    get bible books request

---
+++ BibleQuiz.Application/Features/BibleBooks/Query/GetBibleBooks/GetBibleBooksRequest.cs
@@ -0,0 +1,7 @@
+﻿using BibleQuiz.Domain.Shared;
+using MediatR;
+
+namespace BibleQuiz.Application.Features.BibleBooks.Query.GetBibleBooks;
+public class GetBibleBooksRequest : IRequest<Result<List<GetBibleBooksResponse>, Error>>
+{
+}
