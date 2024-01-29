d8fb0bb52199b38273f63508d85a4277de9c3876
Wale <onafesowale@gmail.com>
2024-01-29 04:17:11 +0100

    modified status code

--- BibleQuiz.Application/Errors/ApplicationError.cs
+++ BibleQuiz.Application/Errors/ApplicationError.cs
@@ -1,10 +1,12 @@
-﻿using BibleQuiz.Domain.Shared;
+﻿using BibleQuiz.Application.Constants;
+using BibleQuiz.Domain.Shared;
 
 namespace BibleQuiz.Application.Errors;
 internal static class ApplicationError
 {
     internal static class TestQuestion
     {
-        internal static Error NotFound(int id) => new("404", $"Question with Id:{id} not found");
+        internal static Error NotFound(int id) => new(ApplicationStatusCodes.NOTFOUND, 
+            $"Question with Id:{id} not found");
     }
 }
