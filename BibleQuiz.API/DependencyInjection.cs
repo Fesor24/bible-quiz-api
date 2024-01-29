e620a194970fe7ee926aca7d1eeb52202502ff02
Wale <onafesowale@gmail.com>
2024-01-29 04:15:02 +0100

    dependency injection

---
+++ BibleQuiz.API/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿namespace BibleQuiz.API;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        services.AddMediatR(opt =>
+        {
+            opt.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
+        });
+
+        return services;
+    }
+}
