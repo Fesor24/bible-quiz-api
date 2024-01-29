bfd2dedaef08d7a35b639c4d6601923fbcc3215f
Wale <onafesowale@gmail.com>
2024-01-29 04:18:22 +0100

    get bible books request handler

---
+++ BibleQuiz.Application/Features/BibleBooks/Query/GetBibleBooks/GetBibleBooksRequestHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using BibleQuiz.Domain.Entities;
+using BibleQuiz.Domain.Primitives;
+using BibleQuiz.Domain.Shared;
+using MediatR;
+
+namespace BibleQuiz.Application.Features.BibleBooks.Query.GetBibleBooks;
+internal sealed class GetBibleBooksRequestHandler : IRequestHandler<GetBibleBooksRequest, 
+    Result<List<GetBibleBooksResponse>, Error>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public GetBibleBooksRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<Result<List<GetBibleBooksResponse>, Error>> Handle(GetBibleBooksRequest request, 
+        CancellationToken cancellationToken)
+    {
+        var books = await _unitOfWork.Repository<BibleBook>().GetAllAsync();
+
+        return _mapper.Map<List<GetBibleBooksResponse>>(books);
+    }
+}
