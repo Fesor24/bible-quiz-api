7f2775d97eb13529a1d54ab54540f5777287f079
Wale <onafesowale@gmail.com>
2024-01-29 04:20:39 +0100

    modified response type

--- BibleQuiz.Application/Features/TestQuestion/Queries/GetQuestions/GetQuestionResponse.cs
+++ BibleQuiz.Application/Features/TestQuestion/Queries/GetQuestions/GetQuestionResponse.cs
@@ -1,10 +1,8 @@
-﻿using BibleQuiz.Domain.Enums;
-
-namespace BibleQuiz.Application.Features.TestQuestion.Queries.GetQuestions;
+﻿namespace BibleQuiz.Application.Features.TestQuestion.Queries.GetQuestions;
 public class GetQuestionResponse
 {
     public string Question { get; set; }
     public string Answer { get; set; }
     public string Verse { get; set; }
-    public QuestionSource Source { get; set; }
+    public string Source { get; set; }
 }
