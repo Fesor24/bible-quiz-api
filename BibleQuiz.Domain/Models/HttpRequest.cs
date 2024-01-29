bfabbcd4b8793c9abc4172d83ed17dd72de396b5
Wale <onafesowale@gmail.com>
2024-01-29 04:29:26 +0100

    Http request

---
+++ BibleQuiz.Domain/Models/HttpRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace BibleQuiz.Domain.Models;
+public class HttpRequest
+{
+    public string Uri { get; set; }
+
+    public HttpMethod Method { get; set; }
+
+    public Dictionary<string, string> Headers { get; set; } = new();
+}
+
+public class HttpRequest<TBody> : HttpRequest
+{
+    public TBody Content { get; set; }
+}
