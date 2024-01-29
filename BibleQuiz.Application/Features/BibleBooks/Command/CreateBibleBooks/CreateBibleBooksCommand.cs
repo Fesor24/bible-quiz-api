eb71f6025b2f108418217944ee9b464419b50bf9
Wale <onafesowale@gmail.com>
2024-01-29 04:17:41 +0100

    create bible books command

---
+++ BibleQuiz.Application/Features/BibleBooks/Command/CreateBibleBooks/CreateBibleBooksCommand.cs
@@ -0,0 +1,7 @@
+﻿using BibleQuiz.Domain.Shared;
+using MediatR;
+
+namespace BibleQuiz.Application.Features.BibleBooks.Command.CreateBibleBooks;
+public class CreateBibleBooksCommand : IRequest<Result<Unit, Error>>
+{
+}
