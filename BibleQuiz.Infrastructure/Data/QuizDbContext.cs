2841691df249e64782ca63e41295ad87b498a613
Wale <onafesowale@gmail.com>
2024-01-30 06:49:28 +0100

    apply entity configs

--- BibleQuiz.Infrastructure/Data/QuizDbContext.cs
+++ BibleQuiz.Infrastructure/Data/QuizDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using BibleQuiz.Domain.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace BibleQuiz.Infrastructure.Data;
 public class QuizDbContext : DbContext
@@ -17,4 +18,11 @@ public class QuizDbContext : DbContext
     public DbSet<Verse> Verse => Set<Verse>();
 
     public DbSet<BibleBook> BibleBook => Set<BibleBook>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
 }
