c2ff5fc873a25aa7706a893598c6db79a675617f
Wale <onafesowale@gmail.com>
2024-01-30 06:49:12 +0100

    migration files

--- BibleQuiz.Infrastructure/Data/Migrations/20240129154418__question-passage-prop.Designer.cs
+++ BibleQuiz.Infrastructure/Data/Migrations/20240129154418__question-passage-prop.Designer.cs
@@ -2,6 +2,7 @@
 using BibleQuiz.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BibleQuiz.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(QuizDbContext))]
-    partial class QuizDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240129154418__question-passage-prop")]
+    partial class _questionpassageprop
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BibleQuiz.Infrastructure/Data/Migrations/20240129154418__question-passage-prop.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BibleQuiz.Infrastructure.Data.Migrations
+{
+    public partial class _questionpassageprop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TestQuestions",
+                table: "TestQuestions");
+
+            migrationBuilder.EnsureSchema(
+                name: "com");
+
+            migrationBuilder.RenameTable(
+                name: "Verse",
+                newName: "Verse",
+                newSchema: "com");
+
+            migrationBuilder.RenameTable(
+                name: "BibleBook",
+                newName: "BibleBook",
+                newSchema: "com");
+
+            migrationBuilder.RenameTable(
+                name: "TestQuestions",
+                newName: "TestQuestion",
+                newSchema: "com");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Source",
+                schema: "com",
+                table: "TestQuestion",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Passage",
+                schema: "com",
+                table: "TestQuestion",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TestQuestion",
+                schema: "com",
+                table: "TestQuestion",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TestQuestion",
+                schema: "com",
+                table: "TestQuestion");
+
+            migrationBuilder.DropColumn(
+                name: "Passage",
+                schema: "com",
+                table: "TestQuestion");
+
+            migrationBuilder.RenameTable(
+                name: "Verse",
+                schema: "com",
+                newName: "Verse");
+
+            migrationBuilder.RenameTable(
+                name: "BibleBook",
+                schema: "com",
+                newName: "BibleBook");
+
+            migrationBuilder.RenameTable(
+                name: "TestQuestion",
+                schema: "com",
+                newName: "TestQuestions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Source",
+                table: "TestQuestions",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TestQuestions",
+                table: "TestQuestions",
+                column: "Id");
+        }
+    }
+}
