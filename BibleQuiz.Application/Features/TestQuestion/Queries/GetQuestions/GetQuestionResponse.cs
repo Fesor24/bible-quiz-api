e98b2400319b7bfedbb9fe0bee0230343d644338
Wale <onafesowale@gmail.com>
2024-01-30 06:45:14 +0100

    Added props to response obj

--- BibleQuiz.Application/Features/TestQuestion/Queries/GetQuestions/GetQuestionResponse.cs
+++ BibleQuiz.Application/Features/TestQuestion/Queries/GetQuestions/GetQuestionResponse.cs
@@ -1,8 +1,10 @@
 ﻿namespace BibleQuiz.Application.Features.TestQuestion.Queries.GetQuestions;
 public class GetQuestionResponse
 {
+    public int Id { get; set; }
     public string Question { get; set; }
     public string Answer { get; set; }
     public string Verse { get; set; }
     public string Source { get; set; }
+    public string Passage { get; set; }
 }
