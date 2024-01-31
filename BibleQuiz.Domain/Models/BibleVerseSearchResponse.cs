392bf07606c969b499b85db58c3a56810d31904d
Wale <onafesowale@gmail.com>
2024-01-31 08:31:08 +0100

    modified object

--- BibleQuiz.Domain/Models/BibleVerseSearchResponse.cs
+++ BibleQuiz.Domain/Models/BibleVerseSearchResponse.cs
@@ -1,46 +1,46 @@
 ﻿namespace BibleQuiz.Domain.Models;
 public class BibleVerseSearchResponse
 {
-    public ScriptureData Data { get; set; }
+    public ScriptureData Data { get; set; } = new();
 
     public ScriptureMeta Meta { get; set; }
+}
 
-    public class ScriptureData
-    {
-        public List<Passages> Passages { get; set; }
-    }
+public class ScriptureData
+{
+    public List<Passages> Passages { get; set; } = new();
+}
 
-    public class Passages
-    {
-        public string Id { get; set; }
+public class Passages
+{
+    public string Id { get; set; }
 
-        public string OrgId { get; set; }
+    public string OrgId { get; set; }
 
-        public string BibleId { get; set; }
+    public string BibleId { get; set; }
 
-        public string BookId { get; set; }
+    public string BookId { get; set; }
 
-        public string[] ChapterIds { get; set; }
+    public string[] ChapterIds { get; set; }
 
-        public string Reference { get; set; }
+    public string Reference { get; set; }
 
-        public string Content { get; set; }
+    public string Content { get; set; }
 
-        public int VerseCount { get; set; }
+    public int VerseCount { get; set; }
 
-        public string Copyright { get; set; }
-    }
+    public string Copyright { get; set; }
+}
 
-    public class ScriptureMeta
-    {
-        public string Fums { get; set; }
+public class ScriptureMeta
+{
+    public string Fums { get; set; }
 
-        public string FumsId { get; set; }
+    public string FumsId { get; set; }
 
-        public string FumsJsInclude { get; set; }
+    public string FumsJsInclude { get; set; }
 
-        public string FumsJs { get; set; }
+    public string FumsJs { get; set; }
 
-        public string FumsNoScript { get; set; }
-    }
+    public string FumsNoScript { get; set; }
 }
