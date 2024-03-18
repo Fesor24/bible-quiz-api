bf1a0407a6fc765464d6abd8dea041c02a7985d5
Wale <onafesowale@gmail.com>
2024-03-18 10:41:47 +0100

    entity configuration

---
+++ BibleQuiz.Infrastructure/Data/EntityConfigurations/Identity/RoleEntityConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using BibleQuiz.Domain.Entities.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BibleQuiz.Infrastructure.Data.EntityConfigurations.Identity;
+internal sealed class RoleEntityConfiguration : IEntityTypeConfiguration<Role>
+{
+    public void Configure(EntityTypeBuilder<Role> builder)
+    {
+        builder.ToTable(nameof(Role), QuizDbContext.USER);
+    }
+}
---
+++ BibleQuiz.Infrastructure/Data/EntityConfigurations/Identity/UserEntityConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using BibleQuiz.Domain.Entities.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BibleQuiz.Infrastructure.Data.EntityConfigurations.Identity;
+internal sealed class UserEntityConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable(nameof(User), QuizDbContext.USER);
+    }
+}
