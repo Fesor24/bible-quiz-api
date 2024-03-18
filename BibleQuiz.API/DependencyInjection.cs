ca9ee18ce5f120143dcea6b6bb76867a38978119
Wale <onafesowale@gmail.com>
2024-03-18 10:38:05 +0100

    DI

--- BibleQuiz.API/DependencyInjection.cs
+++ BibleQuiz.API/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿namespace BibleQuiz.API;
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BibleQuiz.API;
 
 public static class DependencyInjection
 {
@@ -9,6 +11,37 @@ public static class DependencyInjection
             opt.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
         });
 
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", options =>
+            {
+                options.AllowAnyHeader();
+                options.AllowAnyMethod();
+                options.AllowAnyOrigin();
+            });
+        });
+
+        services.Configure<ApiBehaviorOptions>(options =>
+        {
+            options.InvalidModelStateResponseFactory = actionContext =>
+            {
+                var errors = actionContext.ModelState.Where(x => x.Value != null && x.Value.Errors.Count > 0)
+                .SelectMany(x => x.Value.Errors)
+                .Select(x => x.ErrorMessage).ToArray();
+
+                ProblemDetails problem = new();
+
+                problem.Title = "An error occurred";
+                problem.Status = 400;
+                problem.Detail = string.Join(",", errors);
+
+                return new BadRequestObjectResult(problem);
+            };
+        });
+
+
         return services;
+
+
     }
 }
