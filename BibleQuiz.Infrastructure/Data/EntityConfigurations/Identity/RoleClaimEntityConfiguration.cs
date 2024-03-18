8b6a9528b12c0b5f01dd7c83760bae986c7a2075
Wale <onafesowale@gmail.com>
2024-03-18 10:41:35 +0100

    entity configuration

---
+++ BibleQuiz.Infrastructure/Data/EntityConfigurations/Identity/RoleClaimEntityConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using BibleQuiz.Domain.Entities.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BibleQuiz.Infrastructure.Data.EntityConfigurations.Identity;
+internal sealed class RoleClaimEntityConfiguration : IEntityTypeConfiguration<RoleClaim>
+{
+    public void Configure(EntityTypeBuilder<RoleClaim> builder)
+    {
+        builder.ToTable(nameof(RoleClaim), QuizDbContext.USER);
+    }
+}
