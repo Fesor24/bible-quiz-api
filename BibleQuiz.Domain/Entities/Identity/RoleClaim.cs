8b24317dd117c287dfd9acee2e3c6b2d97cdbac9
Wale <onafesowale@gmail.com>
2024-03-18 10:40:02 +0100

    role claim entity

---
+++ BibleQuiz.Domain/Entities/Identity/RoleClaim.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BibleQuiz.Domain.Entities.Identity;
+public class RoleClaim : IdentityRoleClaim<string>
+{
+    public string Description { get; set; }
+    public string Group { get; set; }
+}
