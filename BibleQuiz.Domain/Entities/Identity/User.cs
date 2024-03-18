c6282a372e687f61fc1266ea874d4ea043cfc27f
Wale <onafesowale@gmail.com>
2024-03-18 10:40:19 +0100

    user entity

---
+++ BibleQuiz.Domain/Entities/Identity/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BibleQuiz.Domain.Entities.Identity;
+public class User : IdentityUser
+{
+    public string RefreshToken { get; set; }
+    public DateTime RefreshTokenExpiry { get; set; }
+    public bool IsActive { get; set; }
+}
