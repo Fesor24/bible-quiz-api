392b65aa783f325264fd5ea3af55eb09207e449f
Wale <onafesowale@gmail.com>
2024-03-18 20:23:52 +0100

    App feature

---
+++ BibleQuiz.Shared/Authorization/AppFeature.cs
@@ -0,0 +1,9 @@
+﻿namespace BibleQuiz.Shared.Authorization;
+public static class AppFeature
+{
+    public const string Questions = nameof(Questions);
+    public const string Users = nameof(Users);
+    public const string Roles = nameof(Roles);
+    public const string UserRoles = nameof(UserRoles);
+    public const string RoleClaims = nameof(RoleClaims);
+}
