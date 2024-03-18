58ffe315aa1c68572af5980eddbc90cbdf9e2b15
Wale <onafesowale@gmail.com>
2024-03-18 20:24:29 +0100

    App roles

---
+++ BibleQuiz.Shared/Authorization/AppRoles.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+
+namespace BibleQuiz.Shared.Authorization;
+public static class AppRoles
+{
+    public const string Admin = nameof(Admin);
+    public const string User = nameof(User);
+
+    public static IReadOnlyList<string> Default = 
+        new ReadOnlyCollection<string>(new List<string> { Admin, User });
+
+    public static bool IsDefault(string roleName) =>
+        Default.Any(x => x == roleName);
+}
