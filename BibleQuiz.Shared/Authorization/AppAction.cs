203372c0094f5672ea0d4b8c9de192d5a176b7e4
Wale <onafesowale@gmail.com>
2024-03-18 20:23:38 +0100

    App action

---
+++ BibleQuiz.Shared/Authorization/AppAction.cs
@@ -0,0 +1,8 @@
+﻿namespace BibleQuiz.Shared.Authorization;
+public static class AppAction
+{
+    public const string Create = nameof(Create);
+    public const string Update = nameof(Update);
+    public const string Delete = nameof(Delete);
+    public const string Read = nameof(Read);
+}
