22502a718c4f47ef88c44ad3aa2cf8958075cf2a
Wale <onafesowale@gmail.com>
2024-03-19 17:24:50 +0100

    PermissionAuthorizationHandler

--- BibleQuiz.API/Permissions/PermissionAuthorizationHandler.cs
+++ BibleQuiz.API/Permissions/PermissionAuthorizationHandler.cs
@@ -1,5 +1,25 @@
-﻿namespace BibleQuiz.API.Permissions;
+﻿using BibleQuiz.Shared.Authorization;
+using Microsoft.AspNetCore.Authorization;
 
-public class PermissionAuthorizationHandler
+namespace BibleQuiz.API.Permissions;
+
+public class PermissionAuthorizationHandler : AuthorizationHandler<PermissionRequirement>
 {
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, 
+        PermissionRequirement requirement)
+    {
+        if (context.User is null)
+            return Task.CompletedTask;
+
+        var permissions = context.User.Claims
+            .Where(x => x.Type == AppClaim.Permission && 
+            x.Issuer == "LOCAL AUTHORITY" &&
+            x.Value == requirement.Permission)
+            .ToList();
+
+        if (permissions.Any())
+            context.Succeed(requirement);
+
+        return Task.CompletedTask;
+    }
 }
