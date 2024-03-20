8712148f6295d3536181c1cd761e3e927894ac9e
Wale <onafesowale@gmail.com>
2024-03-20 18:06:59 +0100

    removed props from user entity

--- BibleQuiz.Domain/Entities/Identity/User.cs
+++ BibleQuiz.Domain/Entities/Identity/User.cs
@@ -3,7 +3,5 @@
 namespace BibleQuiz.Domain.Entities.Identity;
 public class User : IdentityUser
 {
-    public string RefreshToken { get; set; }
-    public DateTime RefreshTokenExpiry { get; set; }
     public bool IsActive { get; set; }
 }
