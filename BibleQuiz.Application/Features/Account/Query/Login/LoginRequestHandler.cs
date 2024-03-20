70cf16693859c99f7cc51c66324f898863cacb3b
Wale <onafesowale@gmail.com>
2024-03-20 18:06:33 +0100

    login request handler

---
+++ BibleQuiz.Application/Features/Account/Query/Login/LoginRequestHandler.cs
@@ -0,0 +1,11 @@
+﻿using BibleQuiz.Domain.Shared;
+using MediatR;
+
+namespace BibleQuiz.Application.Features.Account.Query.Login;
+internal sealed class LoginRequestHandler : IRequestHandler<LoginRequest, Result<LoginResponse, Error>>
+{
+    public Task<Result<LoginResponse, Error>> Handle(LoginRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
