f95db17ffde62de5e6228b86394de4fc34ce16e1
Wale <onafesowale@gmail.com>
2024-03-20 18:06:21 +0100

    login request

---
+++ BibleQuiz.Application/Features/Account/Query/Login/LoginRequest.cs
@@ -0,0 +1,5 @@
+﻿using BibleQuiz.Domain.Shared;
+using MediatR;
+
+namespace BibleQuiz.Application.Features.Account.Query.Login;
+public record LoginRequest(string UserName, string Password) : IRequest<Result<LoginResponse, Error>>;
