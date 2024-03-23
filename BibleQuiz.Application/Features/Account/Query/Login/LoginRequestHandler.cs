784b10771ce81b4e4fce1f60db78e70958c8667a
Wale <onafesowale@gmail.com>
2024-03-23 11:25:53 +0100

    login user request handler

--- BibleQuiz.Application/Features/Account/Query/Login/LoginRequestHandler.cs
+++ BibleQuiz.Application/Features/Account/Query/Login/LoginRequestHandler.cs
@@ -1,11 +1,24 @@
-﻿using BibleQuiz.Domain.Shared;
+﻿using BibleQuiz.Domain.Services;
+using BibleQuiz.Domain.Shared;
 using MediatR;
 
 namespace BibleQuiz.Application.Features.Account.Query.Login;
 internal sealed class LoginRequestHandler : IRequestHandler<LoginRequest, Result<LoginResponse, Error>>
 {
-    public Task<Result<LoginResponse, Error>> Handle(LoginRequest request, CancellationToken cancellationToken)
+    private readonly ISecurityService _securityService;
+
+    public LoginRequestHandler(ISecurityService securityService)
     {
-        throw new NotImplementedException();
+        _securityService = securityService;
+    }
+
+    public async Task<Result<LoginResponse, Error>> Handle(LoginRequest request, CancellationToken cancellationToken)
+    {
+        var res = await _securityService.CreateTokenAsync(request.UserName, request.Password);
+
+        if (res.IsFailure)
+            return res.Error;
+
+        return new LoginResponse(request.UserName, res.Value);
     }
 }
