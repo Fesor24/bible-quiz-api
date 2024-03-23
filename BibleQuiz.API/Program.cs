02472909ee695527ca989dc294765e159ebb7073
Wale <onafesowale@gmail.com>
2024-03-23 17:17:35 +0100

    remove svc registration

--- BibleQuiz.API/Program.cs
+++ BibleQuiz.API/Program.cs
@@ -28,7 +28,6 @@ namespace BibleQuiz.API
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
