d266870748e6e9636b7b774b16ed721101044232
Wale <onafesowale@gmail.com>
2024-03-23 17:17:23 +0100

    configure swagger gen

--- BibleQuiz.API/DependencyInjection.cs
+++ BibleQuiz.API/DependencyInjection.cs
@@ -1,6 +1,7 @@
 ﻿using BibleQuiz.API.Permissions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
 
 namespace BibleQuiz.API;
 
@@ -45,8 +46,55 @@ internal static class DependencyInjection
 
         services.AddSingleton<IAuthorizationPolicyProvider, PermissionPolicyProvider>();
 
+        AddSwaggerGen(services);
+
         return services;
+    }
 
 
+    private static IServiceCollection AddSwaggerGen(IServiceCollection services)
+    {
+        services.AddSwaggerGen(opt =>
+        {
+            opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                Description = "Input your Bearer token in this format - Bearer {your token here} to access this resource"
+            });
+
+            opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer",
+                        },
+                        Scheme = "Bearer",
+                        Name = "Bearer",
+                        In = ParameterLocation.Header,
+                    }, new List<string>()
+                }
+            });
+
+            opt.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "Bible Quiz API",
+                License = new OpenApiLicense
+                {
+                    Name = "MIT License",
+                    Url = new Uri("https://opensource.org/licenses/MIT")
+                }
+            });
+        });
+
+        return services;
     }
 }
