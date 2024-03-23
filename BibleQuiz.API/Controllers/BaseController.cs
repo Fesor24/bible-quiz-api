7a11a585973b939b8c91700341f3f4249d47b407
Wale <onafesowale@gmail.com>
2024-03-23 11:52:34 +0100

    Base controller

---
+++ BibleQuiz.API/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BibleQuiz.API.Controllers;
+
+[ApiController]
+public class BaseController<TController> : ControllerBase
+{
+    private ISender _sender;
+
+    protected ISender Sender => _sender ??= 
+        HttpContext.RequestServices.GetService<ISender>();
+}
