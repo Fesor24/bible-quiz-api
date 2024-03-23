ad6f95a585b1839cae2035aedbb11bda4303d930
Wale <onafesowale@gmail.com>
2024-03-23 11:26:24 +0100

    create user command

---
+++ BibleQuiz.Application/Features/Account/Command/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,6 @@
+﻿using BibleQuiz.Domain.Shared;
+using MediatR;
+
+namespace BibleQuiz.Application.Features.Account.Command.CreateUser;
+public record CreateUserCommand(string UserName, string Password) : 
+    IRequest<Result<CreateUserResponse, Error>>;
