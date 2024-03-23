3faff0394d4d0d3eb3adf6c654a8a6bc43de7c66
Wale <onafesowale@gmail.com>
2024-03-23 11:25:25 +0100

    ISecurityService

---
+++ BibleQuiz.Domain/Services/ISecurityService.cs
@@ -0,0 +1,12 @@
+﻿using BibleQuiz.Domain.Entities.Identity;
+using BibleQuiz.Domain.Shared;
+
+namespace BibleQuiz.Domain.Services;
+public interface ISecurityService
+{
+    Task<Result<User, Error>> VerifyUserCredentials(string userName, string password);
+
+    Task<Result<string, Error>> CreateTokenAsync(string userName, string password);
+
+    Task<Result<string, Error>> CreateUserAsync(string userName, string password);
+}
