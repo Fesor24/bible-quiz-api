e04c2d7d21249aaaf1791a2a685890e4530a7b3e
Wale <onafesowale@gmail.com>
2024-03-23 11:52:47 +0100

    inherit from base controller

--- BibleQuiz.API/Controllers/BibleBooksController.cs
+++ BibleQuiz.API/Controllers/BibleBooksController.cs
@@ -2,27 +2,23 @@
 using BibleQuiz.Application.Features.BibleBooks.Command.CreateBibleBooks;
 using BibleQuiz.Application.Features.BibleBooks.Query.GetBibleBooks;
 using BibleQuiz.Domain.Services;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BibleQuiz.API.Controllers;
 
-[ApiController]
-public class BibleBooksController : ControllerBase
+public class BibleBooksController : BaseController<BibleBooksController>
 {
-    private readonly ISender _sender;
     private readonly IBibleService _bibleService;
 
-    public BibleBooksController(ISender sender, IBibleService bibleService)
+    public BibleBooksController(IBibleService bibleService)
     {
-        _sender = sender;
         _bibleService = bibleService;
     }
 
     [HttpGet("/api/kjv-bible-books")]
     public async Task<IActionResult> GetBibleBooks()
     {
-        var res = await _sender.Send(new GetBibleBooksRequest());
+        var res = await Sender.Send(new GetBibleBooksRequest());
 
         return res.Match(val => Ok(val), this.HandleErrorResult);
     }
@@ -36,7 +32,7 @@ public class BibleBooksController : ControllerBase
     [HttpGet("api/kjv/load/bible-books")]
     public async Task<IActionResult> LaodBibleBooks()
     {
-        var res = await _sender.Send(new CreateBibleBooksCommand());
+        var res = await Sender.Send(new CreateBibleBooksCommand());
 
         return res.Match(value => Ok(value), this.HandleErrorResult);
     }
