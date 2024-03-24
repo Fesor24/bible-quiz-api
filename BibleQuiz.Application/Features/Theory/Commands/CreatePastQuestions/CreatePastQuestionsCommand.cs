2027943fd02fe225af7d2a19b509117f1d75c3c1
Wale <onafesowale@gmail.com>
2024-03-24 20:10:59 +0100

    create questions command

---
+++ BibleQuiz.Application/Features/Theory/Commands/CreatePastQuestions/CreatePastQuestionsCommand.cs
@@ -0,0 +1,19 @@
+﻿using BibleQuiz.Domain.Enums;
+using BibleQuiz.Domain.Shared;
+using MediatR;
+
+namespace BibleQuiz.Application.Features.Theory.Commands.CreatePastQuestions;
+public class CreatePastQuestionsCommand : IRequest<Result<Unit, Error>>
+{
+    public List<PastQuestion> Questions { get; set; } = new();
+    public QuestionSource Source { get; set; }
+}
+
+public class PastQuestion 
+{ 
+    public int Id { get; set; }
+    public string Question { get; set; } = string.Empty; 
+    public string Answer { get; set; } = string.Empty; 
+}
+
+
