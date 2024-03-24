b200f50ab4e2a86820c4adc3f9cb3d1fd7e1ed7a
Wale <onafesowale@gmail.com>
2024-03-24 20:14:39 +0100

    response objects

---
+++ BibleQuiz.Application/Features/Theory/Queries/GetQuestions/GetQuestionResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace BibleQuiz.Application.Features.Theory.Queries.GetQuestions;
+public class GetQuestionResponse
+{
+    public int Id { get; set; }
+    public string Question { get; set; }
+    public string Answer { get; set; }
+    public string Verse { get; set; }
+    public string Source { get; set; }
+    public string Passage { get; set; }
+}
