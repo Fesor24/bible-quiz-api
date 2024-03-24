2bf2904fbde907bff8231c9084a4de6773e6ed8a
Wale <onafesowale@gmail.com>
2024-03-24 20:09:27 +0100

    moved class

--- BibleQuiz.Application/Features/TestQuestion/Commands/DeleteQuestion/DeleteQuestionCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿using BibleQuiz.Domain.Shared;
-using MediatR;
-
-namespace BibleQuiz.Application.Features.TestQuestion.Commands.DeleteQuestion;
-public class DeleteQuestionCommand : IRequest<Result<Unit, Error>>
-{
-    public int Id { get; set; }
-}
