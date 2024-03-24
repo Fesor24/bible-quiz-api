e400d83625bd0a96141a4f4f2e797d9842f0187d
Wale <onafesowale@gmail.com>
2024-03-24 20:13:51 +0100

    entity config

---
+++ BibleQuiz.Infrastructure/Data/EntityConfigurations/ObjectiveEntityConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using BibleQuiz.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BibleQuiz.Infrastructure.Data.EntityConfigurations;
+internal sealed class ObjectiveEntityConfiguration : IEntityTypeConfiguration<Objective>
+{
+    public void Configure(EntityTypeBuilder<Objective> builder)
+    {
+        builder.ToTable(nameof(Objective), QuizDbContext.COMMON);
+
+        builder.HasKey(x => x.Id);
+
+        builder.OwnsOne(x => x.Options)
+            .WithOwner();
+
+
+    }
+}
