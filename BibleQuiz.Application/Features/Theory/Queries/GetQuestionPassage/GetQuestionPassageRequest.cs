b370869012635911e9b997d89d3985a985f77f65
Wale <onafesowale@gmail.com>
2024-03-24 20:15:29 +0100

    get question passage functionality

---
+++ BibleQuiz.Application/Features/Theory/Queries/GetQuestionPassage/GetQuestionPassageRequest.cs
@@ -0,0 +1,5 @@
+﻿using BibleQuiz.Domain.Shared;
+using MediatR;
+
+namespace BibleQuiz.Application.Features.Theory.Queries.GetQuestionPassage;
+public record GetQuestionPassageRequest(int QuestionId) : IRequest<Result<GetQuestionPassageResponse, Error>>;
