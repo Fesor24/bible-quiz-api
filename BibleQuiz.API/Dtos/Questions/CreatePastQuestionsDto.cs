5953245722aef24141147f387d02b47c93424f64
Wale <onafesowale@gmail.com>
2024-03-24 20:07:59 +0100

    create past wuestions dto

---
+++ BibleQuiz.API/Dtos/Questions/CreatePastQuestionsDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BibleQuiz.API.Dtos.Questions;
+
+public class CreatePastQuestionsDto
+{
+    public int Id { get; set; }
+    public string Question { get; set; } = string.Empty;
+    public string Answer { get; set; } = string.Empty;
+}
