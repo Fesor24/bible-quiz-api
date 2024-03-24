39144af0f9d56ac2105fdaae680607d50ec506c9
Wale <onafesowale@gmail.com>
2024-03-24 20:13:59 +0100

    entity config

---
+++ BibleQuiz.Infrastructure/Data/EntityConfigurations/TheoryEntityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using BibleQuiz.Domain.Entities;
+using BibleQuiz.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BibleQuiz.Infrastructure.Data.EntityConfigurations;
+internal sealed class TheoryEntityConfiguration : IEntityTypeConfiguration<Theory>
+{
+    public void Configure(EntityTypeBuilder<Theory> builder)
+    {
+        builder.ToTable(nameof(Theory), QuizDbContext.COMMON);
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Source)
+            .HasConversion(sr => sr.ToString(),
+            c => (QuestionSource)Enum.Parse(typeof(QuestionSource), c));
+
+        builder.Property(x => x.Passage)
+            .IsRequired(false);
+    }
+}
