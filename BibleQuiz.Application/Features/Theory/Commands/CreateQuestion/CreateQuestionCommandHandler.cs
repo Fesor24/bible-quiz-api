e7c12c14177e902deb8e77b623377af97ea88d37
Wale <onafesowale@gmail.com>
2024-03-24 20:11:13 +0100

    create questions

---
+++ BibleQuiz.Application/Features/Theory/Commands/CreateQuestion/CreateQuestionCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using TestQuestions = BibleQuiz.Domain.Entities.Theory;
+using BibleQuiz.Domain.Primitives;
+using BibleQuiz.Domain.Shared;
+using MediatR;
+
+namespace BibleQuiz.Application.Features.Theory.Commands.CreateQuestion;
+internal sealed class CreateQuestionCommandHandler :
+    IRequestHandler<CreateQuestionCommand, Result<int, Error>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreateQuestionCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Result<int, Error>> Handle(CreateQuestionCommand request, CancellationToken cancellationToken)
+    {
+        var res = TestQuestions.Create(request.Question, request.Answer, Domain.Enums.QuestionSource.Author,
+            request.Verse);
+
+        if (res.IsFailure)
+            return res.Error;
+
+        await _unitOfWork.Repository<TestQuestions>().AddAsync(res.Value);
+
+        await _unitOfWork.Complete();
+
+        return res.Value.Id;
+    }
+}
---
+++ BibleQuiz.Application/Features/Theory/Commands/CreateQuestions/CreateQuestionsCommand.cs
@@ -0,0 +1,49 @@
+﻿using BibleQuiz.Domain.Shared;
+using FluentValidation;
+using MediatR;
+
+namespace BibleQuiz.Application.Features.Theory.Commands.CreateQuestions;
+public class CreateQuestionsCommand : IRequest<Result<Unit, Error>>
+{
+    public List<CreateQuestionDto> Questions { get; set; }
+}
+
+public class CreateQuestionsCommandValidator : AbstractValidator<CreateQuestionsCommand>
+{
+    public CreateQuestionsCommandValidator()
+    {
+        RuleFor(x => x.Questions)
+            .NotEmpty()
+            .WithMessage("Add at least one question");
+
+        RuleFor(x => x.Questions)
+            .NotNull()
+            .WithMessage("Questions can not be null");
+
+        RuleForEach(x => x.Questions)
+            .ChildRules(question =>
+            {
+                question.RuleFor(q => q.Question)
+                    .NotNull()
+                    .NotEmpty()
+                    .WithMessage("Question can not be null or empty");
+
+                question.RuleFor(q => q.Answer)
+                    .NotNull()
+                    .NotEmpty()
+                    .WithMessage("Answer can not be null or empty");
+
+                question.RuleFor(q => q.Verse)
+                    .NotNull()
+                    .NotEmpty()
+                    .WithMessage("Verse can not be null or empty");
+            });
+    }
+}
+
+public sealed class CreateQuestionDto
+{
+    public string Question { get; set; }
+    public string Answer { get; set; }
+    public string Verse { get; set; }
+}
