090876ce487a5b4c6fdff270f444ec837e631ccb
Wale <onafesowale@gmail.com>
2024-03-24 20:07:48 +0100

    create objective question dto

---
+++ BibleQuiz.API/Dtos/Questions/CreateObjectiveQuestionDto.cs
@@ -0,0 +1,4 @@
+﻿namespace BibleQuiz.API.Dtos.Questions;
+
+public record CreateObjectiveQuestionDto(string Question, 
+    string OptionA, string OptionB, string OptionC, string OptionD, string Answer);
