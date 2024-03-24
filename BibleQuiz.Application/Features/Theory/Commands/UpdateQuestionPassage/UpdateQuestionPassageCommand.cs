32e32759631f3df5a88413fd363a87ff366d980c
Wale <onafesowale@gmail.com>
2024-03-24 20:15:00 +0100

    update question passage

---
+++ BibleQuiz.Application/Features/Theory/Commands/UpdateQuestionPassage/UpdateQuestionPassageCommand.cs
@@ -0,0 +1,14 @@
+﻿using BibleQuiz.Domain.Shared;
+using MediatR;
+
+namespace BibleQuiz.Application.Features.Theory.Commands.UpdateQuestionPassage;
+public class UpdateQuestionPassageCommand : IRequest<Result<Unit, Error>>
+{
+    public UpdateQuestionPassageDto Data { get; set; }
+}
+
+public class UpdateQuestionPassageDto
+{
+    public int QuestionId { get; set; }
+    public string Passage { get; set; }
+}
