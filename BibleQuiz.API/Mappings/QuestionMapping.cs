ca99f4ade29e18b0c8b9ff23714df6d7e61b926d
Wale <onafesowale@gmail.com>
2024-03-24 20:08:08 +0100

    question mapping

---
+++ BibleQuiz.API/Mappings/QuestionMapping.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using BibleQuiz.API.Dtos.Questions;
+using BibleQuiz.Application.Features.Theory.Commands.CreatePastQuestions;
+
+namespace BibleQuiz.API.Mappings;
+
+public class QuestionMapping : Profile
+{
+    public QuestionMapping()
+    {
+        CreateMap<CreatePastQuestionsDto, PastQuestion>();
+    }
+}
