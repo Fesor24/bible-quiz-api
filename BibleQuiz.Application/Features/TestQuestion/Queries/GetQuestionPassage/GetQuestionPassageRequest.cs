f717b0e6b4b5ebc00c8d1b57fefc77c7ff6ba542
Wale <onafesowale@gmail.com>
2024-03-24 20:09:41 +0100

    moved class

--- BibleQuiz.Application/Features/TestQuestion/Queries/GetQuestionPassage/GetQuestionPassageRequest.cs
+++
@@ -1,5 +0,0 @@
-﻿using BibleQuiz.Domain.Shared;
-using MediatR;
-
-namespace BibleQuiz.Application.Features.TestQuestion.Queries.GetQuestionPassage;
-public record GetQuestionPassageRequest(int QuestionId) : IRequest<Result<GetQuestionPassageResponse, Error>>;
