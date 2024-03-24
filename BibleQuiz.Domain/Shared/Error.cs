61119969af5af0f216cfe17328bff7c1d13b7767
Wale <onafesowale@gmail.com>
2024-03-24 20:13:25 +0100

    added methods

--- BibleQuiz.Domain/Shared/Error.cs
+++ BibleQuiz.Domain/Shared/Error.cs
@@ -12,5 +12,9 @@ public class Error
 
     public static readonly Error None = new(string.Empty, string.Empty);
 
+    public static Error Validation(string code, string message) => new(code, message);
+    public static Error NotFound(string code, string message) => new(code, message);
+    public static Error Conflict(string code, string message) => new(code, message);
+
     public static implicit operator Result(Error error) => Result.Failure(error);
 }
