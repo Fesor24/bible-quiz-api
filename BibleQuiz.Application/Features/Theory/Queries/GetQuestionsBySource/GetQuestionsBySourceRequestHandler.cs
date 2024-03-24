2d571fd5cd9366f45da02a2291f8f8a00aa132d2
Wale <onafesowale@gmail.com>
2024-03-24 20:14:29 +0100

    get questions by source functionality

---
+++ BibleQuiz.Application/Features/Theory/Queries/GetQuestionsBySource/GetQuestionsBySourceRequestHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using TestQuestions = BibleQuiz.Domain.Entities.Theory;
+using BibleQuiz.Domain.Primitives;
+using BibleQuiz.Domain.Shared;
+using MediatR;
+using BibleQuiz.Domain.Specifications;
+using BibleQuiz.Application.Features.Theory.Queries.GetQuestions;
+
+namespace BibleQuiz.Application.Features.Theory.Queries.GetQuestionsBySource;
+internal sealed class GetQuestionsBySourceRequestHandler : IRequestHandler<GetQuestionsBySourceRequest,
+    Result<IReadOnlyList<GetQuestionResponse>, Error>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public GetQuestionsBySourceRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<Result<IReadOnlyList<GetQuestionResponse>, Error>> Handle(GetQuestionsBySourceRequest request,
+        CancellationToken cancellationToken)
+    {
+        var spec = new GetTestQuestionsBySourceSpecification(request.Source);
+
+        var res = await _unitOfWork.Repository<TestQuestions>().GetAllWithSpecAsync(spec);
+
+        if (res is null)
+            return new List<GetQuestionResponse>();
+
+        return _mapper.Map<List<GetQuestionResponse>>(res);
+    }
+
+}
