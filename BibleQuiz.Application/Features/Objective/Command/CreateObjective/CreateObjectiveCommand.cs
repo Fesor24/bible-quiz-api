d3fc781390a67a7c5d0c0aed1fe07f74015ee4f9
Wale <onafesowale@gmail.com>
2024-03-24 20:08:20 +0100

    create objective command

---
+++ BibleQuiz.Application/Features/Objective/Command/CreateObjective/CreateObjectiveCommand.cs
@@ -0,0 +1,12 @@
+﻿using BibleQuiz.Domain.Shared;
+using MediatR;
+
+namespace BibleQuiz.Application.Features.Objective.Command.CreateObjective;
+public record CreateObjectiveCommand(
+    string Question,
+    string OptionA,
+    string OptionB,
+    string OptionC,
+    string OptionD,
+    string Answer
+    ) : IRequest<Result<int, Error>>;
