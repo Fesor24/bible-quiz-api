513b347e69a11fd08f4a2b999ce1ae1d5a3fb1e6
Wale <onafesowale@gmail.com>
2024-03-25 16:47:50 +0100

    global usings

--- BibleQuiz.Application/GlobalUsing.cs
+++ BibleQuiz.Application/GlobalUsing.cs
@@ -1,1 +1,3 @@
 ﻿global using ObjectiveEntity = BibleQuiz.Domain.Entities.Objective;
+global using TheoryEntity = BibleQuiz.Domain.Entities.Theory;
+global using BibleBookEntity = BibleQuiz.Domain.Entities.BibleBook;
