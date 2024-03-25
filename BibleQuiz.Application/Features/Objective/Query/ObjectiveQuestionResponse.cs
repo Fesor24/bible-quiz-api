619b53ecb8c84415d8249f806e8d5dc1b2964e2a
Wale <onafesowale@gmail.com>
2024-03-25 16:46:19 +0100

    objective question response

---
+++ BibleQuiz.Application/Features/Objective/Query/ObjectiveQuestionResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace BibleQuiz.Application.Features.Objective.Query;
+public class ObjectiveQuestionResponse
+{
+    public string Question { get; set; }
+    public string OptionA { get; set; }
+    public string OptionB { get; set; }
+    public string OptionC { get; set; }
+    public string OptionD { get; set; }
+    public string Answer { get; set; }
+}
