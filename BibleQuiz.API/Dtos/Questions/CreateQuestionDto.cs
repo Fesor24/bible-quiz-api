a7617bc23405e4bad62519550de34698ce35f2e6
Wale <onafesowale@gmail.com>
2024-03-25 16:44:45 +0100

    create question dto

---
+++ BibleQuiz.API/Dtos/Questions/CreateQuestionDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BibleQuiz.API.Dtos.Questions;
+
+public class CreateQuestionDto 
+{ 
+    public string Question { get; set; } 
+    public string Answer { get; set; } 
+    public string Book { get; set; }
+    public int VerseFrom { get; set; }
+    public int VerseTo { get; set;}
+    public int Chapter { get; set; }
+ }
