dec085abd3ae41fbeea20f99292c6b97b5b04a9c
Wale <onafesowale@gmail.com>
2024-03-25 16:46:08 +0100

    get all objective functionalities

---
+++ BibleQuiz.Application/Features/Objective/Query/GetAllObjectiveQuestionsQuery.cs
@@ -0,0 +1,6 @@
+﻿using BibleQuiz.Domain.Shared;
+using MediatR;
+
+namespace BibleQuiz.Application.Features.Objective.Query;
+public record GetAllObjectiveQuestionsQuery : 
+    IRequest<Result<IReadOnlyList<ObjectiveQuestionResponse>, Error>>;
---
+++ BibleQuiz.Application/Features/Objective/Query/GetAllObjectiveQuestionsQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using BibleQuiz.Domain.Primitives;
+using BibleQuiz.Domain.Shared;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibleQuiz.Application.Features.Objective.Query;
+internal sealed class GetAllObjectiveQuestionsQueryHandler : 
+    IRequestHandler<GetAllObjectiveQuestionsQuery, Result<IReadOnlyList<ObjectiveQuestionResponse>, Error>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public GetAllObjectiveQuestionsQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<Result<IReadOnlyList<ObjectiveQuestionResponse>, Error>> Handle(GetAllObjectiveQuestionsQuery request,
+        CancellationToken cancellationToken)
+    {
+        var objectives = await _unitOfWork.Repository<ObjectiveEntity>().GetAllAsync();
+
+        var res = _mapper.Map<IReadOnlyList<ObjectiveQuestionResponse>>(objectives);
+
+        return new Result<IReadOnlyList<ObjectiveQuestionResponse>, Error>(res);
+    }
+        
+}
