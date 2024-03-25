2ccb5e6202e9d369f6b49bc41d3441ee175b39f5
Wale <onafesowale@gmail.com>
2024-03-25 16:48:22 +0100

    extend Error class

--- BibleQuiz.Domain/Shared/Error.cs
+++ BibleQuiz.Domain/Shared/Error.cs
@@ -12,9 +12,38 @@ public class Error
 
     public static readonly Error None = new(string.Empty, string.Empty);
 
-    public static Error Validation(string code, string message) => new(code, message);
-    public static Error NotFound(string code, string message) => new(code, message);
-    public static Error Conflict(string code, string message) => new(code, message);
+    public static ValidationError Validation(string code, string message) => new(code, message);
+    public static NotFoundError NotFound(string code, string message) => new(code, message);
+
+    public static ConflictError Conflict(string code, string message) => new(code, message);
+
+    public static bool IsNotFound { get; private set; }
+    public static bool IsValidation { get; private set; }
+    public static bool IsConflict { get; private set; }
 
     public static implicit operator Result(Error error) => Result.Failure(error);
+}
+
+public class ValidationError : Error
+{
+    public ValidationError(string code, string message): base(code, message)
+    {
+        
+    }
+}
+
+public class NotFoundError: Error
+{
+    public NotFoundError(string code, string message) : base(code, message)
+    {
+        
+    }
+}
+
+public class ConflictError : Error
+{
+    public ConflictError(string code, string message) : base(code, message)
+    {
+
+    }
 }
