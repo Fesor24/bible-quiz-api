3b310408ae96f6bfe93973584aab6e9ea0ddc025
Wale <onafesowale@gmail.com>
2024-03-25 16:48:02 +0100

    questions mappings

---
+++ BibleQuiz.Application/MappingProfiles/QuestionsMappings.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using BibleQuiz.Application.Features.Objective.Query;
+using BibleQuiz.Application.Features.Theory.Commands.CreateQuestions;
+using BibleQuiz.Application.Features.Theory.Queries.GetQuestions;
+using BibleQuiz.Domain.Entities;
+
+namespace BibleQuiz.Application.MappingProfiles;
+public sealed class QuestionsMappings : Profile
+{
+    public QuestionsMappings()
+    {
+        CreateMap<Theory, GetQuestionResponse>();
+
+        CreateMap<CreateQuestion, Theory>();
+
+        CreateMap<ObjectiveEntity, ObjectiveQuestionResponse>()
+            .ForMember(c => c.OptionA, d => d.MapFrom(s => s.Options.OptionA))
+            .ForMember(c => c.OptionB, d => d.MapFrom(s => s.Options.OptionB))
+            .ForMember(c => c.OptionC, d => d.MapFrom(s => s.Options.OptionC))
+            .ForMember(c => c.OptionD, d => d.MapFrom(s => s.Options.OptionD));
+    }
+}
